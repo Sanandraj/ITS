0ce8bec1dffc7326160632ec0070642f21e31f3b
Sanandraj <96609211+Sanandraj@users.noreply.github.com>
2022-07-19 21:11:11 +0530

    Add files via upload

---
+++ EDI_CUTOFF/ITSVesselVisitFormSubmission.groovy
@@ -0,0 +1,64 @@
+import com.navis.external.framework.ui.AbstractFormSubmissionCommand
+import com.navis.external.framework.util.EFieldChange
+import com.navis.external.framework.util.EFieldChanges
+import com.navis.framework.business.Roastery
+import com.navis.framework.metafields.MetafieldId
+import com.navis.framework.metafields.MetafieldIdFactory
+import com.navis.framework.metafields.entity.EntityId
+import com.navis.framework.persistence.hibernate.CarinaPersistenceCallback
+import com.navis.framework.persistence.hibernate.PersistenceTemplate
+import com.navis.framework.presentation.FrameworkPresentationUtils
+import com.navis.inventory.business.api.InventoryCargoManager
+import com.navis.vessel.business.schedule.VesselVisitDetails
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+
+import java.text.SimpleDateFormat
+
+class ITSVesselVisitFormSubmission extends AbstractFormSubmissionCommand {
+    @Override
+    void doBeforeSubmit(String inVariformId, EntityId inEntityId, List<Serializable> inGkeys, EFieldChanges inOutFieldChanges, EFieldChanges inNonDbFieldChanges, Map<String, Object> inParams) {
+        LOGGER.setLevel(Level.DEBUG)
+        LOGGER.debug("ITSVesselVisitFormSubmission execution begins::::")
+        boolean isValid = false
+        PersistenceTemplate persistenceTemplate = new PersistenceTemplate(FrameworkPresentationUtils.getUserContext())
+        persistenceTemplate.invoke(new CarinaPersistenceCallback() {
+            @Override
+            protected void doInTransaction() {
+                if (inGkeys != null && inGkeys.size() > 0) {
+                    InventoryCargoManager inventoryCargoManager = (InventoryCargoManager) Roastery.getBean(InventoryCargoManager.BEAN_ID);
+
+                    for (Serializable gkey : inGkeys) {
+                        VesselVisitDetails vesselVisitDetails = VesselVisitDetails.hydrate(gkey)
+                          EFieldChange eFieldChange_cargo = inOutFieldChanges.findFieldChange(CARGO_CUT_OFF)
+                        EFieldChange eFieldChange_Edi = inOutFieldChanges.findFieldChange(CARGO_EDI_OFF)
+                        if (eFieldChange_cargo != null) {
+                            Date cargoCut = eFieldChange_cargo.getNewValue() as Date
+
+                            def sdf = new SimpleDateFormat("yyyy-MM-dd")
+                            if (vesselVisitDetails.getVvFlexDate02() == null && cargoCut != null && eFieldChange_Edi == null) {
+                                LOGGER.debug("ITSVesselVisitFormSubmission cargoCut is full and haz is mty ::::")
+                                isValid = true;
+                            } else {
+                                LOGGER.debug("ITSVesselVisitFormSubmission cargoCut is mty and haz is mty ::::")
+                                isValid = false;
+                            }
+                        }
+                    }
+                    if (isValid) {
+                        registerError("Cannot update CARGO-cut-off field because EDI-cut-off field is mandatory")
+                    }
+
+
+                }
+
+            }
+        });
+    }
+
+
+    private static Logger LOGGER = Logger.getLogger(ITSVesselVisitFormSubmission.class)
+    private static final MetafieldId CARGO_CUT_OFF = MetafieldIdFactory.valueOf("vvdTimeCargoCutoff")
+    private static final MetafieldId CARGO_EDI_OFF = MetafieldIdFactory.valueOf("vvFlexDate02")
+}
+
