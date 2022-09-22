86af87b7ed0cb559f5c5667e133ea20fd8e46cd4
AarthiUmaSankar <41193938+AarthiUmaSankar@users.noreply.github.com>
2022-09-22 14:55:17 +0530

    YARD_REHANDLE - Record REHANDLE Event when moved from Non-deliverable to Deliverable block

--- Enhancements/ITSRecordYardRehandleGeneralNotice.groovy
+++ Enhancements/ITSRecordYardRehandleGeneralNotice.groovy
@@ -8,7 +8,9 @@ import com.navis.framework.portal.FieldChanges
 import com.navis.inventory.business.api.UnitField
 import com.navis.inventory.business.units.Unit
 import com.navis.inventory.business.units.UnitFacilityVisit
+import com.navis.orders.OrdersField
 import com.navis.services.business.event.Event
+import com.navis.services.business.event.EventFieldChange
 import com.navis.services.business.event.GroovyEvent
 import com.navis.services.business.rules.EventType
 import org.apache.log4j.Logger
@@ -23,21 +25,41 @@ class ITSRecordYardRehandleGeneralNotice extends AbstractGeneralNoticeCodeExtens
     void execute(GroovyEvent inGroovyEvent) {
         super.execute(inGroovyEvent)
 
-        Event event = inGroovyEvent.getEvent()
+        log.warn("Fieldd changes "+inGroovyEvent.getEvent().getEvntFieldChanges())
+
+        for (EventFieldChange efc : inGroovyEvent.getEvent().getEvntFieldChanges()) {
+            log.warn("Field Id:" + efc.getMetafieldId());
+            log.warn("Field Value:" + efc.getPrevVal());
+
+        }
+        //return
         Unit unit = (Unit) inGroovyEvent.getEntity()
-        String prevPosition = inGroovyEvent.getPropertyAsString("PositionSlot")
+        String prevPosition = inGroovyEvent.getPreviousPropertyAsString("PositionFull")
         log.warn("prevPosition "+prevPosition)
-        if(unit.getUnitActiveUfvNowActive()){
+
+
+        if(unit.getUnitActiveUfvNowActive() && prevPosition){
             UnitFacilityVisit ufv = unit.getUnitActiveUfvNowActive()
             LocPosition position = ufv.getUfvLastKnownPosition()
 
             LocPosition prevPositionLoc = null
             if (prevPosition != null) {
+                if(prevPosition.indexOf('Y-PIERG-') != -1){
+                    prevPosition = prevPosition.replaceAll('Y-PIERG-','')
+                }
                 prevPositionLoc = LocPosition.resolvePosition(ContextHelper.getThreadFacility(), LocTypeEnum.YARD, ContextHelper.getThreadYardId(), prevPosition, null, unit.getUnitEquipment().getEqEquipType().getEqtypBasicLength())
             }
-            log.warn("prevPositionLoc "+prevPositionLoc)
-            log.warn("pos block name "+position.getBlockName())
-            if(position.getBlockName() != null && isBlockDeliverable(position.getBlockName()) && (!isBlockDeliverable(prevPositionLoc.getBlockName()))){// CONFIRM prevPositionLoc == null ||
+
+            String prevblockName = prevPositionLoc.getBlockName()
+            if (prevblockName == null && prevPositionLoc.getPosSlot() != null && prevPositionLoc.getPosSlot().indexOf('.') != -1) {
+                prevblockName = prevPositionLoc.getPosSlot().split('\\.')[0]
+            }
+
+            String blockName = position.getBlockName()
+            if (blockName == null && position.getPosSlot() != null && position.getPosSlot().indexOf('.') != -1) {
+                blockName = position.getPosSlot().split('\\.')[0]
+            }
+            if(blockName != null && isBlockDeliverable(blockName) && prevblockName && (!isBlockDeliverable(prevblockName))){// CONFIRM prevPositionLoc == null ||
                 EventType yardRehandle = EventType.findEventType("UNIT_YARD_REHANDLE")
                 FieldChanges fc = new FieldChanges()
                 fc.setFieldChange(UnitField.POS_SLOT, prevPosition, position.getPosSlot())
@@ -48,8 +70,6 @@ class ITSRecordYardRehandleGeneralNotice extends AbstractGeneralNoticeCodeExtens
 
     boolean isBlockDeliverable(String blkId) {
         GeneralReference genRef = GeneralReference.findUniqueEntryById("ITS", "DELIVERABLE_BLOCK", blkId)
-        log.warn("prevPositionLoc "+blkId)
-        log.warn("genRef "+genRef.getRefValue1())
 
         if (genRef != null && genRef.getRefValue1().equalsIgnoreCase("Y")) {
             return true
