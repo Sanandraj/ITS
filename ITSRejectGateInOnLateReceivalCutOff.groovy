852935fc9b23b67971a4a463d14f92ce17a32a34
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2022-09-22 18:47:28 +0530

    Update ITSRejectGateInOnLateReceivalCutOff.groovy

--- ITSRejectGateInOnLateReceivalCutOff.groovy
+++ ITSRejectGateInOnLateReceivalCutOff.groovy
@@ -1,23 +1,22 @@
 import com.navis.argo.ContextHelper
 import com.navis.argo.business.api.ArgoUtils
 import com.navis.external.road.AbstractGateTaskInterceptor
-import com.navis.framework.util.internationalization.PropertyKeyFactory
-import com.navis.framework.util.message.MessageLevel
 import com.navis.road.business.model.TruckTransaction
-import com.navis.road.business.model.TruckVisitDetails
 import com.navis.road.business.workflow.TransactionAndVisitHolder
 import com.navis.vessel.business.schedule.VesselVisitDetails
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
+/**
+ *  @Author <a href="mailto:skishore@weservetech.com">Kishore Kumar S</a>
+ */
+
 class ITSRejectGateInOnLateReceivalCutOff extends AbstractGateTaskInterceptor {
     @Override
     void execute(TransactionAndVisitHolder inWfCtx) {
-        executeInternal(inWfCtx)
         LOGGER.setLevel(Level.DEBUG)
-        LOGGER.debug( "ITSRejectGateInOnLateReceivalCutOff STARTS::")
+        LOGGER.debug( "b STARTS::")
         TruckTransaction tran = inWfCtx.getTran()
-        TruckVisitDetails truckDetails = inWfCtx.getTv()
         if (tran.getTranAppointment().getGapptVesselVisit() != null){
             Serializable vvd = tran.getTranAppointment().getGapptVesselVisit().getCvGkey()
             LOGGER.debug("vvd :: "+vvd)
@@ -26,13 +25,8 @@ class ITSRejectGateInOnLateReceivalCutOff extends AbstractGateTaskInterceptor {
             TimeZone timeZone = ContextHelper.getThreadUserTimezone();
             LOGGER.debug("timeZone :: "+timeZone)
             if (vesselVisitDetails.getVvFlexDate01().after(ArgoUtils.convertDateToLocalDateTime(ArgoUtils.timeNow(), timeZone))){
+                executeInternal(inWfCtx)
                 LOGGER.debug("***** correctTime Loop *****")
-                tran.recordStageCompleted("ingate")
-                LOGGER.debug("*** Saved Transaction ***")
-            }
-            else {
-                LOGGER.debug("Inside past cutoff time")
-                getMessageCollector().appendMessage(MessageLevel.SEVERE, PropertyKeyFactory.valueOf("Late Receival CutOff Past"),"Please contact Administration")
             }
         }
     }
