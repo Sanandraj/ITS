72141ed7ccb4ac4c7f936425c5ee880325fe2a44
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2022-09-22 18:40:51 +0530

    Update ITSCompleteDropOffOutgate.groovy

--- ITSCompleteDropOffOutgate.groovy
+++ ITSCompleteDropOffOutgate.groovy
@@ -1,66 +1,34 @@
 import com.navis.argo.ContextHelper
-import com.navis.argo.business.reference.Container
-import com.navis.argo.business.reference.Equipment
+import com.navis.argo.business.api.ArgoUtils
 import com.navis.external.road.AbstractGateTaskInterceptor
-import com.navis.framework.business.Roastery
-import com.navis.framework.metafields.MetafieldIdFactory
-import com.navis.inventory.business.api.UnitFinder
-import com.navis.inventory.business.units.Unit
-import com.navis.inventory.business.units.UnitFacilityVisit
-import com.navis.road.business.atoms.TranStatusEnum
-import com.navis.road.business.atoms.TransactionClassEnum
 import com.navis.road.business.model.TruckTransaction
-import com.navis.road.business.model.TruckVisitDetails
 import com.navis.road.business.workflow.TransactionAndVisitHolder
+import com.navis.vessel.business.schedule.VesselVisitDetails
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
 /**
- * @Author <a href="mailto:skishore@weservetech.com">KISHORE KUMAR S</a>
+ *  @Author <a href="mailto:skishore@weservetech.com">Kishore Kumar S</a>
  */
-class ITSCompleteDropOffOutGate extends AbstractGateTaskInterceptor{
-    private static final Logger LOGGER = Logger.getLogger(ITSCompleteDropOffOutGate.class)
+
+class ITSRejectGateInOnLateReceivalCutOff extends AbstractGateTaskInterceptor {
+    @Override
     void execute(TransactionAndVisitHolder inWfCtx) {
         LOGGER.setLevel(Level.DEBUG)
-        LOGGER.debug("ITSCompleteDropOffOutGate Starts::")
-        TruckTransaction truckTransaction = inWfCtx.getTran()
-        if (truckTransaction != null && truckTransaction.isDelivery()) {
-            TruckVisitDetails truckVisitDetails = truckTransaction.getTranTruckVisit()
-            LOGGER.debug("truckVisitDetails :: "+truckVisitDetails)
-            if (truckVisitDetails != null) {
-                Set<TruckTransaction> dropOffContainers = truckVisitDetails.getTransactionsToBeHandled(TransactionClassEnum.DROPOFF)
-                LOGGER.debug("dropOffContainers :: "+dropOffContainers)
-                for (TruckTransaction tran : dropOffContainers) {
-                    if(TranStatusEnum.OK.equals(tran.getTranStatus())) {
-                        tran.setTranStatus(TranStatusEnum.COMPLETE)
-                        String containerId= tran.getTranContainer().getEqIdFull()
-                        Container container = Container.findContainer(containerId)
-                        Equipment equipment = container != null ? Equipment.findEquipment(containerId) : null
-                        Unit unit = getFinder().findActiveUnit(ContextHelper.getThreadComplex(),equipment)
-                        LOGGER.debug("Unit :: "+unit)
-                        if (unit!=null){
-                            unit.setFieldValue(MetafieldIdFactory.valueOf("unitFlexString04"),"Yes")
-                            LOGGER.debug("containerId :: "+containerId)
-                            LOGGER.debug("ITSCompleteDropOffOutGate Code Ends")
-                        }
-                        
-                    }else {
-                        String containerId= tran.getTranContainer().getEqIdFull()
-                        Container container = Container.findContainer(containerId)
-                        Equipment equipment = container != null ? Equipment.findEquipment(containerId) : null
-                        Unit unit = getFinder().findActiveUnit(ContextHelper.getThreadComplex(),equipment)
-                        LOGGER.debug("Unit :: "+unit)
-                        if (unit!=null){
-                            unit.setFieldValue(MetafieldIdFactory.valueOf("unitFlexString04"),"No")
-                            LOGGER.debug("containerId :: "+containerId)
-                            LOGGER.debug("ITSCompleteDropOffOutGate Code Ends")
-                        }
-                    }
-                }
+        LOGGER.debug( "b STARTS::")
+        TruckTransaction tran = inWfCtx.getTran()
+        if (tran.getTranAppointment().getGapptVesselVisit() != null){
+            Serializable vvd = tran.getTranAppointment().getGapptVesselVisit().getCvGkey()
+            LOGGER.debug("vvd :: "+vvd)
+            VesselVisitDetails vesselVisitDetails = VesselVisitDetails.hydrate(vvd)
+            LOGGER.debug("vesselVisitDetails :: "+vesselVisitDetails)
+            TimeZone timeZone = ContextHelper.getThreadUserTimezone();
+            LOGGER.debug("timeZone :: "+timeZone)
+            if (vesselVisitDetails.getVvFlexDate01().after(ArgoUtils.convertDateToLocalDateTime(ArgoUtils.timeNow(), timeZone))){
+                executeInternal(inWfCtx)
+                LOGGER.debug("***** correctTime Loop *****")
             }
         }
     }
-    private static UnitFinder getFinder() {
-        return (UnitFinder) Roastery.getBean(UnitFinder.BEAN_ID);
-    }
+    private static Logger LOGGER = Logger.getLogger(ITSRejectGateInOnLateReceivalCutOff.class)
 }
