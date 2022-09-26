c27bca93914b60f1cc24a13321dfbd5af7a0a093
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2022-09-26 09:43:02 +0530

    Update ITSRejectGateInOnLateReceivalCutOff.groovy

--- ITSRejectGateInOnLateReceivalCutOff.groovy
+++ ITSRejectGateInOnLateReceivalCutOff.groovy
@@ -1,5 +1,7 @@
 import com.navis.argo.ContextHelper
 import com.navis.argo.business.api.ArgoUtils
+import com.navis.argo.business.api.GroovyApi
+import com.navis.argo.business.model.CarrierVisit
 import com.navis.external.road.AbstractGateTaskInterceptor
 import com.navis.road.business.model.TruckTransaction
 import com.navis.road.business.workflow.TransactionAndVisitHolder
@@ -15,18 +17,26 @@ class ITSRejectGateInOnLateReceivalCutOff extends AbstractGateTaskInterceptor {
     @Override
     void execute(TransactionAndVisitHolder inWfCtx) {
         LOGGER.setLevel(Level.DEBUG)
-        LOGGER.debug( "b STARTS::")
+        LOGGER.debug( "ITSRejectGateInOnLateReceivalCutOff STARTS::")
         TruckTransaction tran = inWfCtx.getTran()
         if (tran.getTranAppointment().getGapptVesselVisit() != null){
-            Serializable vvd = tran.getTranAppointment().getGapptVesselVisit().getCvGkey()
-            LOGGER.debug("vvd :: "+vvd)
-            VesselVisitDetails vesselVisitDetails = VesselVisitDetails.hydrate(vvd)
-            LOGGER.debug("vesselVisitDetails :: "+vesselVisitDetails)
-            TimeZone timeZone = ContextHelper.getThreadUserTimezone();
-            LOGGER.debug("timeZone :: "+timeZone)
-            if (vesselVisitDetails.getVvFlexDate01().after(ArgoUtils.convertDateToLocalDateTime(ArgoUtils.timeNow(), timeZone))){
+            Serializable cvGkey = tran.getTranAppointment().getGapptVesselVisit().getCvGkey()
+            LOGGER.debug("cvGkey :: "+cvGkey)
+            String cv = CarrierVisit.hydrate(cvGkey).getCvId()
+            LOGGER.debug("vesselVisitDetails :: "+cv)
+            VesselVisitDetails vvd= VesselVisitDetails.resolveVvdFromCv(tran.getTranAppointment().getGapptVesselVisit())
+            TimeZone timeZone = ContextHelper.getThreadUserTimezone()
+            if (vvd.getVvFlexDate01() != null){
+                if ((ArgoUtils.convertDateToLocalDateTime(ArgoUtils.timeNow(), timeZone)).after(vvd.getVvFlexDate01())){
+                    LOGGER.debug("Inside after cutoff")
+                    executeInternal(inWfCtx)
+                }
+                /*else {
+                    new GroovyApi().registerError("Contact Administration - Late-Receival Cutoff Past")
+                }*/
+            }
+            else {
                 executeInternal(inWfCtx)
-                LOGGER.debug("***** correctTime Loop *****")
             }
         }
     }
