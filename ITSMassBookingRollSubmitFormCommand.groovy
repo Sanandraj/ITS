23376a48b593e713c3a08f29feb1050bd8116d25
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2022-10-06 19:05:30 +0530

    Update ITSMassBookingRollSubmitFormCommand.groovy

--- ITSMassBookingRollSubmitFormCommand.groovy
+++ ITSMassBookingRollSubmitFormCommand.groovy
@@ -1,6 +1,3 @@
-import com.navis.argo.business.api.ArgoUtils
-import com.navis.argo.business.model.CarrierVisit
-import com.navis.argo.business.reference.ScopedBizUnit
 import com.navis.external.framework.ui.AbstractFormSubmissionCommand
 import com.navis.external.framework.util.EFieldChanges
 import com.navis.framework.business.Roastery
@@ -9,11 +6,7 @@ import com.navis.framework.persistence.hibernate.CarinaPersistenceCallback
 import com.navis.framework.persistence.hibernate.PersistenceTemplate
 import com.navis.framework.presentation.FrameworkPresentationUtils
 import com.navis.inventory.business.api.UnitFinder
-import com.navis.inventory.business.units.Unit
-import com.navis.inventory.business.units.UnitFacilityVisit
 import com.navis.orders.business.eqorders.Booking
-import com.navis.vessel.business.schedule.VesselVisitDetails
-import com.navis.inventory.business.atoms.UfvTransitStateEnum
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
@@ -34,98 +27,42 @@ class ITSMassBookingRollSubmitFormCommand extends AbstractFormSubmissionCommand{
             @Override
             protected void doInTransaction() {
                 final Logger LOGGER = Logger.getLogger(ITSMassBookingRollSubmitFormCommand.class)
-                List<String> bkLineOperator = new ArrayList<String>()
-                List<Serializable> bkgVesselVisit = new ArrayList<String>()
+                LOGGER.debug("Inside Persistence")
+                String constantLineOpr= null
+                String constantVvd = null
+                int count = 0
                 for (Serializable gKey : inGkeys as List<Serializable>){
-                    LOGGER.debug("inside gKey If ")
+                    count = count + 1
                     Booking booking = Booking.hydrate(gKey)
-                    LOGGER.debug("booking :: "+booking)
-                    String bkgLineOp = booking.getEqoLine().getBzuId()
-                    LOGGER.debug("bkgLineOp ::"+bkgLineOp)
-                    bkLineOperator.add(bkgLineOp)
-                    String bkgVvId = booking.getEqoVesselVisit().getCvGkey()
-                    LOGGER.debug("bkgVvId :: "+bkgVvId)
-                    bkgVesselVisit.add(bkgVvId)
-                }
-                LOGGER.debug("bkLineOperator ::"+bkLineOperator)
-                LOGGER.debug("bkgVesselVisit ::"+bkgVesselVisit)
-                if (bkLineOperator !=null && bkLineOperator.size()>0 && bkgVesselVisit != null && bkgVesselVisit.size()>0){
-                    boolean allVvEqual = bkgVesselVisit.stream().distinct().limit(2).count() <= 1
-                    boolean allLineOpEqual = bkLineOperator.stream().distinct().limit(2).count() <= 1
-                    boolean errorValue = false
-
-                    if (allVvEqual){
-                        LOGGER.debug("inside allVv Equal If ")
-                        for (Serializable cvGkey: bkgVesselVisit){
-                            CarrierVisit cvD= CarrierVisit.hydrate(cvGkey)
-                            VesselVisitDetails visitDetails = VesselVisitDetails.resolveVvdFromCv(cvD)
-                            LOGGER.debug("vvd :: "+visitDetails)
-                            for (String lineOp :bkLineOperator ){
-                                LOGGER.debug("lineOp :: "+lineOp)
-                                if (visitDetails.isLineAllowed(ScopedBizUnit.findEquipmentOperator(lineOp))){
-                                    LOGGER.debug("Vessel Visit is shared by Line Operator")
-                                    LOGGER.debug("vvD Cut Off time :: "+visitDetails.getVvdTimeCargoCutoff())
-                                    if ((visitDetails.getVvdTimeCargoCutoff()).after(ArgoUtils.timeNow())){
-                                        for (Serializable gKey : inGkeys as List<Serializable>){
-                                            Booking booking = Booking.hydrate(gKey)
-                                            List<Unit> eqboUnitsNbr = getFinder().findUnitsForOrder(booking)
-                                            LOGGER.debug("eqboUnitsNbr :: "+eqboUnitsNbr)
-                                            if (eqboUnitsNbr!=null && eqboUnitsNbr.size()>0){
-                                                LOGGER.debug("Inside Unit List not null")
-                                                for (Unit unit: eqboUnitsNbr){
-                                                    LOGGER.debug("Inside unit Iteration :: "+unit)
-                                                    for (UnitFacilityVisit ufv : (unit.getUnitUfvSet() as List<UnitFacilityVisit>)){
-                                                        String ufvTransitState= ufv.getUfvTransitState()
-                                                        LOGGER.debug("ufvTransitState :: "+ufvTransitState)
-                                                        if (allLineOpEqual){
-                                                            LOGGER.debug("Inside all Line Operator equal")
-                                                            if ((ufv.getUfvTransitState()).equals(UfvTransitStateEnum.S10_ADVISED)){
-                                                                errorValue = false
-                                                                LOGGER.debug("Successfully Rolled")
-                                                            }
-                                                            else if (!(ufv.getUfvTransitState()).equals(UfvTransitStateEnum.S10_ADVISED) &&
-                                                                    ufv.isTransitStateBeyond(UfvTransitStateEnum.S20_INBOUND) &&
-                                                                    ufv.isTransitStateAtMost(UfvTransitStateEnum.S40_YARD))
-                                                            {
-                                                                errorValue = false
-                                                                LOGGER.debug("Cannot perform booking Roll also no error message")
-                                                            }
-                                                            else if (!(ufv.getUfvTransitState()).equals(UfvTransitStateEnum.S10_ADVISED) &&
-                                                                    ufv.isTransitStateBeyond(UfvTransitStateEnum.S10_ADVISED)){
-                                                                errorValue = false
-                                                                LOGGER.debug("Successfully Rolled")
-                                                            }
-                                                        }
-                                                        /**Same vessel visit - different line operator */
-                                                        else {
-                                                            errorValue = true
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                        /** Cut-off time has crossed*/
-                                    else {
-                                        errorValue = true
-                                    }
-                                }
-                            }
+                    if (count==1){
+                        constantLineOpr = booking.getEqoLine().getBzuId()
+                    }
+                    if (count>1){
+                        String bkgCurrentLineOpr = booking.getEqoLine().getBzuId()
+                        if (!bkgCurrentLineOpr.equals(constantLineOpr) ){
+                            registerError("Error : Cannot perform Booking roll for selected bookings")
+                            return
                         }
                     }
-                    /** different Vessel visit - same and different line operator*/
-                    else {
-                        errorValue = true
+                    if (count==1){
+                        constantVvd = booking.getEqoVesselVisit().getCvId()
                     }
-                    if (errorValue){
+                    if (count>1){
+                        String bkgVvd = booking.getEqoVesselVisit().getCvId()
+                        if (!bkgVvd.equals(constantVvd)){
+                            registerError("Error : Cannot perform Booking roll for selected bookings")
+                            return
+                        }
+                    }
+                    Long bkgTallyReceive = booking.getEqoTallyReceive()
+                    LOGGER.debug("bkgTallyReceive :: "+bkgTallyReceive)
+                    if (bkgTallyReceive > 0 ){
                         registerError("Error : Cannot perform Booking roll for selected bookings")
-                        LOGGER.debug("Do not Roll Booking")
+                        return
                     }
                 }
             }
         })
     }
-    private static UnitFinder getFinder() {
-        return (UnitFinder) Roastery.getBean(UnitFinder.BEAN_ID)
-    }
+    private static UnitFinder getFinder() {return (UnitFinder) Roastery.getBean(UnitFinder.BEAN_ID)}
 }
