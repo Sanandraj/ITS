d05fde55400222649bde795005613f1602984456
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2022-10-27 11:16:10 +0530

    B-2-3  General reference to maintain customer code and service

--- customBeanITSGenerateInvoiceFormController.groovy
+++ customBeanITSGenerateInvoiceFormController.groovy
@@ -1,6 +1,7 @@
 package ITSIntegration
 
 import com.navis.argo.business.model.GeneralReference
+import com.navis.billing.business.model.Customer
 import com.navis.billing.business.model.InvoiceType
 import com.navis.billing.presentation.controller.GenerateInvoiceFormController
 import com.navis.external.framework.beans.EBean
@@ -15,8 +16,11 @@ import com.navis.framework.presentation.ui.command.SubmitFormCommand
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
-class customBeanITSGenerateInvoiceFormController extends GenerateInvoiceFormController implements EBean{
+/**Author: <a href="mailto:skishore@weservetech.com"> KISHORE KUMAR S </a>
+ * Description: This Code will be paste against BEAN_PROTOTYPE Code Extension Type - This code performs updating cutDebitCode value(Customer Number - Customer Entity) by general reference value.
+ **/
 
+class customBeanITSGenerateInvoiceFormController extends GenerateInvoiceFormController implements EBean{
     @Override
     ISubmitFormCommand getCreateUpdateDataCommand(){
         new SubmitFormCommand(this) {
@@ -26,29 +30,25 @@ class customBeanITSGenerateInvoiceFormController extends GenerateInvoiceFormCont
                 LOGGER1.setLevel(Level.DEBUG)
                 LOGGER1.debug("customBeanITSGenerateInvoiceFormController Starts :: ")
                 FieldChanges fieldChange = (FieldChanges) inFieldChanges
-                LOGGER1.debug("FieldChanges ::"+fieldChange)
-                String invTypeNewValueGKEY = fieldChange.getFieldChange(MetafieldIdFactory.valueOf("invoiceInvoiceType")).getNewValue()
-                LOGGER1.debug("invTypeNewValueGKEY ::"+invTypeNewValueGKEY)
-                if (fieldChange.hasFieldChange(MetafieldIdFactory.valueOf("invoiceInvoiceType"))) {
+                String invTypeNewValueGKEY = fieldChange?.getFieldChange(MetafieldIdFactory.valueOf("invoiceInvoiceType"))?.getNewValue()
+                String payeeCustomer = fieldChange?.getFieldChange(MetafieldIdFactory.valueOf("invoicePayeeCustomer"))?.getNewValue()
+                if (fieldChange?.hasFieldChange(MetafieldIdFactory.valueOf("invoiceInvoiceType"))) {
                     if (invTypeNewValueGKEY != null) {
-                        LOGGER1.debug("Inside If:: ")
                         PersistenceTemplate persistenceTemplate = new PersistenceTemplate(FrameworkPresentationUtils.getUserContext())
                         persistenceTemplate.invoke(new CarinaPersistenceCallback() {
                             @Override
                             protected void doInTransaction() {
                                 InvoiceType invType = InvoiceType.hydrate(invTypeNewValueGKEY as Serializable)
-                                LOGGER1.debug("invType :: "+invType)
                                 GeneralReference gn = GeneralReference.findUniqueEntryById("ITS_CUSTOMER_SERVICE",invType.getInvtypeId())
-                                LOGGER1.debug("Gen Ref ::"+gn)
                                 if (gn!=null){
-                                    fieldChange.setFieldChange(MetafieldIdFactory.valueOf("invoiceInvoiceType"),gn.getRefValue1())
-                                    LOGGER1.debug("Code Ends")
+                                    Customer customer= Customer.hydrate(payeeCustomer as Serializable)
+                                    customer?.setFieldValue(MetafieldIdFactory.valueOf("custDebitCode"),gn?.getRefValue1())
                                 }
                             }
                         })
                     }
                 }
-                getFormUiProcessor().hideWindow(this.getFormController())
+                getFormUiProcessor()?.hideWindow(this.getFormController())
                 return new BizResponse()
             }
         }
