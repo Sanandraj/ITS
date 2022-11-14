18b1678074a0fcd95b4ccebe5d17f560f44abb85
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2022-11-14 16:35:05 +0530

    Update ITSCompleteDropOffOutgate.groovy

--- ITSCompleteDropOffOutgate.groovy
+++ ITSCompleteDropOffOutgate.groovy
@@ -18,14 +18,14 @@ class ITSCompleteDropOffOutGate extends AbstractGateTaskInterceptor{
         LOGGER.setLevel(Level.DEBUG)
         LOGGER.debug("ITSCompleteDropOffOutGate Starts::")
         TruckTransaction truckTransaction = inWfCtx.getTran()
-        if (truckTransaction != null && truckTransaction.isDelivery()) {
-            TruckVisitDetails truckVisitDetails = truckTransaction.getTranTruckVisit()
+        if (truckTransaction != null && truckTransaction?.isDelivery()) {
+            TruckVisitDetails truckVisitDetails = truckTransaction?.getTranTruckVisit()
             if (truckVisitDetails != null) {
-                Set<TruckTransaction> dropOffContainers = truckVisitDetails.getTransactionsToBeHandled(TransactionClassEnum.DROPOFF)
+                Set<TruckTransaction> dropOffContainers = truckVisitDetails?.getTransactionsToBeHandled(TransactionClassEnum.DROPOFF)
                 for (TruckTransaction tran : dropOffContainers) {
-                    Unit unit = tran.getTranUnit()
+                    Unit unit = tran?.getTranUnit()
                     if (unit != null){
-                        if(TranStatusEnum.OK.equals(tran.getTranStatus())) {
+                        if(TranStatusEnum.OK.equals(tran?.getTranStatus())) {
                             tran.setTranStatus(TranStatusEnum.COMPLETE)
                             unit.setFieldValue(MetafieldIdFactory.valueOf("unitFlexString04"),"YES")
                         }else {
