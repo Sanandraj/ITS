5382ccd5458aeeac5a134645f3a65a2f71ffc4fe
Kishore Kumar S <96610767+skishore0709@users.noreply.github.com>
2022-11-15 22:09:07 +0530

    Update ITSMassBookingRollSubmitFormCommand.groovy

--- ITSMassBookingRollSubmitFormCommand.groovy
+++ ITSMassBookingRollSubmitFormCommand.groovy
@@ -1,68 +1,81 @@
+/*
+ * Copyright (c) 2022 WeServe LLC. All Rights Reserved.
+ *
+ */
+
 import com.navis.external.framework.ui.AbstractFormSubmissionCommand
 import com.navis.external.framework.util.EFieldChanges
-import com.navis.framework.business.Roastery
 import com.navis.framework.metafields.entity.EntityId
 import com.navis.framework.persistence.hibernate.CarinaPersistenceCallback
 import com.navis.framework.persistence.hibernate.PersistenceTemplate
 import com.navis.framework.presentation.FrameworkPresentationUtils
-import com.navis.inventory.business.api.UnitFinder
 import com.navis.orders.business.eqorders.Booking
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
 /**
- * @Author <a href="mailto:skishore@weservetech.com">KISHORE KUMAR S</a>
+ * @Author: Kishore Kumar S <a href= skishore@weservetech.com / >, 11/10/2022
+ * Requirements : 5-14-Mass-Booking-Roll -- This groovy is used to roll multiple booking selected from booking entity .
+ * @Inclusion Location	: Incorporated as a code extension of the type FORM_SUBMISSION_INTERCEPTOR.
+ *  Load Code Extension to N4:
+ 1. Go to Administration --> System -->  Code Extension
+ 2. Click Add (+)
+ 3. Enter the values as below:
+ Code Extension Name:  ITSMassBookingRollSubmitFormCommand.
+ Code Extension Type:  FORM_SUBMISSION_INTERCEPTOR.
+ Groovy Code: Copy and paste the contents of groovy code.
+ 4. Click Save button
+ *
+ *  Set up override configuration in variformId - ORD001.
  */
 
 class ITSMassBookingRollSubmitFormCommand extends AbstractFormSubmissionCommand{
     private static Logger LOGGER = Logger.getLogger(ITSMassBookingRollSubmitFormCommand.class)
     @Override
     void doBeforeSubmit(String inVariformId, EntityId inEntityId, List<Serializable> inGkeys, EFieldChanges inOutFieldChanges, EFieldChanges inNonDbFieldChanges, Map<String, Object> inParams) {
-        LOGGER.setLevel(Level.DEBUG)
-        LOGGER.debug("ITSMassBookingRollSubmitFormCommand Starts ::")
-        LOGGER.debug("inGkeys :: "+inGkeys)
-        LOGGER.debug("inOutFieldChanges :: "+inOutFieldChanges)
+        LOGGER.setLevel(Level.INFO)
+        LOGGER.info("ITSMassBookingRollSubmitFormCommand Starts ::")
+        if (inGkeys == null && inGkeys.isEmpty()){
+            return;
+        }
         PersistenceTemplate persistenceTemplate = new PersistenceTemplate(FrameworkPresentationUtils.getUserContext())
         persistenceTemplate.invoke(new CarinaPersistenceCallback() {
             @Override
             protected void doInTransaction() {
-                final Logger LOGGER = Logger.getLogger(ITSMassBookingRollSubmitFormCommand.class)
-                LOGGER.debug("Inside Persistence")
-                String constantLineOpr= null
-                String constantVvd = null
-                int count = 0
+                String constantLineOpr= null;
+                String constantVvd = null;
+                int count = 0;
                 for (Serializable gKey : inGkeys as List<Serializable>){
-                    count = count + 1
-                    Booking booking = Booking.hydrate(gKey)
-                    if (count==1){
-                        constantLineOpr = booking.getEqoLine().getBzuId()
-                    }
-                    if (count>1){
-                        String bkgCurrentLineOpr = booking.getEqoLine().getBzuId()
-                        if (!bkgCurrentLineOpr.equals(constantLineOpr) ){
-                            registerError("Error : Cannot perform Booking roll for selected bookings")
-                            return
-                        }
+                    count = count + 1;
+                    Booking booking = Booking.hydrate(gKey);
+                    if (booking == null){
+                        return;
                     }
                     if (count==1){
-                        constantVvd = booking.getEqoVesselVisit().getCvId()
+                        constantLineOpr = booking.getEqoLine() ? booking.getEqoLine().getBzuId() : null;
+                        constantVvd = booking.getEqoVesselVisit() != null ? booking.getEqoVesselVisit().getCvId() :null;
                     }
                     if (count>1){
-                        String bkgVvd = booking.getEqoVesselVisit().getCvId()
-                        if (!bkgVvd.equals(constantVvd)){
-                            registerError("Error : Cannot perform Booking roll for selected bookings")
-                            return
+                        String bkgCurrentLineOpr = booking.getEqoLine()  != null ? booking.getEqoLine().getBzuId() : null;
+                        if (bkgCurrentLineOpr != null && !bkgCurrentLineOpr.equals(constantLineOpr) ){
+                            registerError("Error : Cannot perform Booking roll for selected bookings");
+                            return;
+                        }
+                        if (booking.getEqoVesselVisit()){
+                            String bkgVvd = booking.getEqoVesselVisit().getCvId();
+                            if (!bkgVvd.equals(constantVvd)){
+                                registerError("Error : Cannot perform Booking roll for selected bookings");
+                                return;
+                            }
                         }
                     }
-                    Long bkgTallyReceive = booking.getEqoTallyReceive()
-                    LOGGER.debug("bkgTallyReceive :: "+bkgTallyReceive)
+                    Long bkgTallyReceive = booking?.getEqoTallyReceive();
                     if (bkgTallyReceive > 0 ){
-                        registerError("Error : Cannot perform Booking roll for selected bookings")
-                        return
+                        registerError("Error : Cannot perform Booking roll for selected bookings");
+                        return;
                     }
                 }
             }
         })
     }
-    private static UnitFinder getFinder() {return (UnitFinder) Roastery.getBean(UnitFinder.BEAN_ID)}
 }
