b2e99f1e917d2b0d8b5c32f564d87211b08e365a
AshokkumarKD <104904543+AshokkumarKD@users.noreply.github.com>
2022-11-19 00:21:30 +0530

    Final Review Done(has backup in local)

--- ITSUpdateUnitsCountToBLGenaralNotice.groovy
+++ ITSUpdateUnitsCountToBLGenaralNotice.groovy
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2022 WeServe LLC. All Rights Reserved.
  *
- */
+*/
+package ITS
 
-import com.navis.argo.business.atoms.EventEnum
 import com.navis.cargo.business.model.BillOfLading
 import com.navis.cargo.business.model.BlGoodsBl
 import com.navis.external.services.AbstractGeneralNoticeCodeExtension
@@ -15,20 +15,35 @@ import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
 /**
- * @Author: Kishore Kumar S && Naresh Kumar <a href= skishore@weservetech.com / > && <a href= mnaresh@weservetech.com / >, 31-10-2022
- * Requirements : TT-17+TT-18 B/L – Stow Cross Reference -- This groovy is used to update count of units against BL .
- * @Inclusion Location	: Incorporated as a code extension of the type GENERAL_NOTICE_CODE_EXTENSION.
- *  Load Code Extension to N4:
- 1. Go to Administration --> System -->  Code Extension
- 2. Click Add (+)
- 3. Enter the values as below:
- Code Extension Name:  ITSUpdateUnitsCountToBLGenaralNotice.
- Code Extension Type:  GENERAL_NOTICE_CODE_EXTENSION.
- Groovy Code: Copy and paste the contents of groovy code.
- 4. Click Save button
+ * Date: 31-10-2022
+ * UNIT_RESERVE - Naresh Kumar
+ * UNIT_CANCEL_RESERVE - Kishore Kumar S
+ * Requirements:-  Updating Bill of Ladding FlexString-01 Value based on number of units associated to BL.
+ * Trigger: UNIT_RESERVE and UNIT_CANCEL_RESERVE
+ * @Inclusion Location	: Incorporated as a code extension of the type GENERAL_NOTICE_CODE_EXTENSION --> Paste this code (ITSUpdateUnitsCountToBLGenaralNotice.groovy)
  *
- *  Set up override configuration in General Notice against UNIT_RESERVE and UNIT_CANCEL_RESERVE events.
- */
+ *
+ *
+ * @Author: mailto:skishore@weservetech.com & mailto:mnaresh@weservetech.com, Kishore Kumar S & Naresh Kumar; Date: 31-10-2022
+ *
+ *  Requirements: Updating Bill of Ladding FlexString-01 Value based on number of units associated to BL.
+ *
+ * @Inclusion Location: Incorporated as a code extension of the type
+ *
+ *  Load Code Extension to N4:
+ *  1. Go to Administration --> System --> Code Extensions
+ *  2. Click Add (+)
+ *  3. Enter the values as below:
+ *     Code Extension Name: ITSUpdateUnitsCountToBLGenaralNotice
+ *     Code Extension Type: GENERAL_NOTICE_CODE_EXTENSION
+ *     Groovy Code: Copy and paste the contents of groovy code.
+ *  4. Click Save button
+ *
+ * @Setup The Events UNIT_RESERVE and UNIT_CANCEL_RESERVE with ITSUpdateUnitsCountToBLGenaralNotice Code Extension
+ *
+ *  S.No    Modified Date   Modified By     Jira      Description
+ *
+ * */
 
 class ITSUpdateUnitsCountToBLGenaralNotice extends AbstractGeneralNoticeCodeExtension {
     public static final String UNIT_RESERVE = "UNIT_RESERVE"
@@ -38,14 +53,14 @@ class ITSUpdateUnitsCountToBLGenaralNotice extends AbstractGeneralNoticeCodeExte
     void execute(GroovyEvent inGroovyEvent) {
         LOGGER.setLevel(Level.DEBUG)
         LOGGER.debug("ITSUpdateUnitsCountToBLGenaralNotice is invoked::")
-        if (EventEnum.UNIT_RESERVE.getKey().equalsIgnoreCase(inGroovyEvent.getEvent()?.getEventTypeId())){
+        if (EventEnum.UNIT_RESERVE.getKey().equalsIgnoreCase(inGroovyEvent.getEvent()?.getEventTypeId())) {
             Unit unit = (Unit) inGroovyEvent.getEntity()
-            if (unit == null){
+            if (unit == null) {
                 return;
             }
             if (unit?.getUnitGoods() != null && unit?.getUnitGoods()?.getGdsBlNbr()) {
                 Collection blGoodsblCollection = inventoryCargoManager1?.getBlGoodsBls(unit)
-                if (blGoodsblCollection != null){
+                if (blGoodsblCollection != null) {
                     for (BlGoodsBl blGoodsBl : (blGoodsblCollection as List<BlGoodsBl>)) {
                         if (blGoodsBl.getBlgdsblBl() != null) {
                             List unitList = inventoryCargoManager1?.findUnitsForBillOfLading(blGoodsBl.getBlgdsblBl())
@@ -58,30 +73,23 @@ class ITSUpdateUnitsCountToBLGenaralNotice extends AbstractGeneralNoticeCodeExte
                     }
                 }
             }
-        }
-        else if (EventEnum.UNIT_CANCEL_RESERVE.getKey().equalsIgnoreCase(inGroovyEvent?.getEvent()?.getEventTypeId())){
+        } else if (EventEnum.UNIT_CANCEL_RESERVE.getKey().equalsIgnoreCase(inGroovyEvent?.getEvent()?.getEventTypeId())) {
             Unit unit = (Unit) inGroovyEvent.getEntity()
-            if (unit == null){
+            if (unit == null) {
                 return;
             }
-            String  evntNote = inGroovyEvent.getEvent()?.getEventNote()
-            if (evntNote != null){
+            String evntNote = inGroovyEvent.getEvent()?.getEventNote()
+            if (evntNote != null) {
                 String blNbr = getLastWordUsingSplit(evntNote);
-                if (blNbr != null && !blNbr.isEmpty())
-                {
-                    List<BillOfLading> billOfLading= BillOfLading.findAllBillsOfLading(blNbr);
-                    for (BillOfLading bl : billOfLading)
-                    {
+                if (blNbr != null && !blNbr.isEmpty()) {
+                    List<BillOfLading> billOfLading = BillOfLading.findAllBillsOfLading(blNbr);
+                    for (BillOfLading bl : billOfLading) {
                         Collection blGoodsblCollection = bl?.getBlBlGoodsBls();
-                        if (blGoodsblCollection!=null)
-                        {
-                            for (BlGoodsBl blGoodsBl : (blGoodsblCollection as List<BlGoodsBl>))
-                            {
-                                if (blGoodsBl.getBlgdsblBl() != null)
-                                {
+                        if (blGoodsblCollection != null) {
+                            for (BlGoodsBl blGoodsBl : (blGoodsblCollection as List<BlGoodsBl>)) {
+                                if (blGoodsBl.getBlgdsblBl() != null) {
                                     List unitList = inventoryCargoManager1.findUnitsForBillOfLading(blGoodsBl?.getBlgdsblBl())
-                                    if (unitList != null && unitList.size() > 0)
-                                    {
+                                    if (unitList != null && unitList.size() > 0) {
                                         blGoodsBl?.getBlgdsblBl()?.setBlFlexString01(String.valueOf(unitList?.size()))
                                     }
                                 }
@@ -93,7 +101,7 @@ class ITSUpdateUnitsCountToBLGenaralNotice extends AbstractGeneralNoticeCodeExte
         }
     }
     private static final Logger LOGGER = Logger.getLogger(ITSUpdateUnitsCountToBLGenaralNotice.class)
-    private static  InventoryCargoManager inventoryCargoManager1 = (InventoryCargoManager) Roastery.getBean(InventoryCargoManager.BEAN_ID)
+    private static InventoryCargoManager inventoryCargoManager1 = (InventoryCargoManager) Roastery.getBean(InventoryCargoManager.BEAN_ID)
 
     private static String getLastWordUsingSplit(String input) {
         String[] tokens = input.split(" ");
