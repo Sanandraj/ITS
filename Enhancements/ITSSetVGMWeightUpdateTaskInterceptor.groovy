6fbe43c17df77af02077d5ccdac3f20474e75c82
Sanandraj <96609211+Sanandraj@users.noreply.github.com>
2022-11-18 16:29:09 +0530

    IP-315-Final Review done.

--- Enhancements/ITSSetVGMWeightUpdateTaskInterceptor.groovy
+++ Enhancements/ITSSetVGMWeightUpdateTaskInterceptor.groovy
@@ -11,8 +11,8 @@ import com.navis.road.business.workflow.TransactionAndVisitHolder
  * @Author <a href="mailto:mmadhavan@weservetech.com">Madhavan M</a>, 10/OCT/2022
  *
  * Requirements : 1. VGM should be applied to the export container  at the time it is successfully processed at the in-gate if VERMAS EDI has been received prior to container arrival.
-                  2. If no VGM has been received for the container when it arrives at the in-gate, then the scale weight is applied as the VGM.
-                  3. When the VERMAS is received, this scale weight is replaced with the VERMAS weight.
+ 2. If no VGM has been received for the container when it arrives at the in-gate, then the scale weight is applied as the VGM.
+ 3. When the VERMAS is received, this scale weight is replaced with the VERMAS weight.
  *
  * @Inclusion Location: Incorporated as a code extension of the type
  *
@@ -37,11 +37,11 @@ class ITSSetVGMWeightUpdateTaskInterceptor extends AbstractGateTaskInterceptor {
     void execute(TransactionAndVisitHolder inWfCtx) {
         super.execute(inWfCtx)
 
-        if(inWfCtx.getTran()== null){
+        if (inWfCtx.getTran() == null) {
             return;
         }
 
-        if(inWfCtx.getTran().getTranCtrVGMWeight()==null){
+        if (inWfCtx.getTran().getTranCtrVGMWeight() == null) {
             inWfCtx.getTran().setTranCtrVGMWeight(inWfCtx.getTran().getTranScaleWeight())
             inWfCtx.getTran().getTranUnit().updateVerifiedGrossMassWtKg(inWfCtx.getTran().getTranScaleWeight())
         }
