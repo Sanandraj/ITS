f68ad73b201d51d60801604cb2a005f529dc76bf
Sanandraj <96609211+Sanandraj@users.noreply.github.com>
2022-11-18 19:36:42 +0530

    IP-304-Final Review done.

--- Enhancements/ITSRecordYardRehandleGeneralNotice.groovy
+++ Enhancements/ITSRecordYardRehandleGeneralNotice.groovy
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022 WeServe LLC. All Rights Reserved.
+ *
+ */
+
+
 import com.navis.argo.ContextHelper
 import com.navis.argo.business.api.ArgoUtils
 import com.navis.argo.business.atoms.LocTypeEnum
@@ -8,44 +14,57 @@ import com.navis.framework.portal.FieldChanges
 import com.navis.inventory.business.api.UnitField
 import com.navis.inventory.business.units.Unit
 import com.navis.inventory.business.units.UnitFacilityVisit
-import com.navis.orders.OrdersField
-import com.navis.services.business.event.Event
-import com.navis.services.business.event.EventFieldChange
 import com.navis.services.business.event.GroovyEvent
 import com.navis.services.business.rules.EventType
 import org.apache.log4j.Logger
 
-/**
- * Billing 7-3 Yard rehandle fee
- * Weserve - 08/17 Record a event UNIT_YARD_REHANDLE when the Unit is moved from a Undeliverable spot to a Deliverable spot.
- * Configured against all Yard move Event triggers.
+/*
+     *
+     *  @Author: mailto:uaarthi@weservetech.com, Aarthi U; Date:17/08/2022
+     *
+     * Requirements : Billing 7-3 Yard rehandle fee.Record a event UNIT_YARD_REHANDLE when the Unit is moved from a Undeliverable spot to a Deliverable spot.
+     *
+     * @Inclusion Location	: Incorporated as a code extension of the type GENERAL_NOTICE_CODE_EXTENSION.
+     *
+     *  Load Code Extension to N4:
+            1. Go to Administration --> System -->  Code Extension
+            2. Click Add (+)
+            3. Enter the values as below:
+                Code Extension Name:  ITSRecordYardRehandleGeneralNotice
+                Code Extension Type:  GENERAL_NOTICE_CODE_EXTENSION
+               Groovy Code: Copy and paste the contents of groovy code.
+            4. Click Save button
+
+    * @Set up General Notice for event types "UNIT_YARD_MOVE,UNIT_POSITION_CORRECTION,UNIT_YARD_SHIFT" on Unit Entity then execute this code extension (ITSRecordYardRehandleGeneralNotice).
+    *
+    *  S.No    Modified Date   Modified By     Jira      Description
+    *
  */
-class ITSRecordYardRehandleGeneralNotice extends AbstractGeneralNoticeCodeExtension{
+
+
+class ITSRecordYardRehandleGeneralNotice extends AbstractGeneralNoticeCodeExtension {
     @Override
     void execute(GroovyEvent inGroovyEvent) {
         super.execute(inGroovyEvent)
 
-        log.warn("Fieldd changes "+inGroovyEvent.getEvent().getEvntFieldChanges())
-
-        for (EventFieldChange efc : inGroovyEvent.getEvent().getEvntFieldChanges()) {
-            log.warn("Field Id:" + efc.getMetafieldId());
-            log.warn("Field Value:" + efc.getPrevVal());
-
-        }
-        //return
         Unit unit = (Unit) inGroovyEvent.getEntity()
+        if (unit == null) {
+            return;
+        }
+
         String prevPosition = inGroovyEvent.getPreviousPropertyAsString("PositionFull")
-        log.warn("prevPosition "+prevPosition)
 
-
-        if(unit.getUnitActiveUfvNowActive() && prevPosition){
+        if (unit.getUnitActiveUfvNowActive() && prevPosition) {
             UnitFacilityVisit ufv = unit.getUnitActiveUfvNowActive()
+            if (ufv == null) {
+                return;
+            }
             LocPosition position = ufv.getUfvLastKnownPosition()
 
             LocPosition prevPositionLoc = null
             if (prevPosition != null) {
-                if(prevPosition.indexOf('Y-PIERG-') != -1){
-                    prevPosition = prevPosition.replaceAll('Y-PIERG-','')
+                if (prevPosition.indexOf('Y-PIERG-') != -1) {
+                    prevPosition = prevPosition.replaceAll('Y-PIERG-', '')
                 }
                 prevPositionLoc = LocPosition.resolvePosition(ContextHelper.getThreadFacility(), LocTypeEnum.YARD, ContextHelper.getThreadYardId(), prevPosition, null, unit.getUnitEquipment().getEqEquipType().getEqtypBasicLength())
             }
@@ -59,11 +78,13 @@ class ITSRecordYardRehandleGeneralNotice extends AbstractGeneralNoticeCodeExtens
             if (blockName == null && position.getPosSlot() != null && position.getPosSlot().indexOf('.') != -1) {
                 blockName = position.getPosSlot().split('\\.')[0]
             }
-            if(blockName != null && isBlockDeliverable(blockName) && prevblockName && (!isBlockDeliverable(prevblockName))){// CONFIRM prevPositionLoc == null ||
+            if (blockName != null && isBlockDeliverable(blockName) && prevblockName && (!isBlockDeliverable(prevblockName))) {
                 EventType yardRehandle = EventType.findEventType("UNIT_YARD_REHANDLE")
-                FieldChanges fc = new FieldChanges()
-                fc.setFieldChange(UnitField.POS_SLOT, prevPosition, position.getPosSlot())
-                unit.recordEvent(yardRehandle, fc, "Unit moved from Undeliverable to Deliverable location", ArgoUtils.timeNow())
+                if (yardRehandle != null) {
+                    FieldChanges fc = new FieldChanges()
+                    fc.setFieldChange(UnitField.POS_SLOT, prevPosition, position.getPosSlot())
+                    unit.recordEvent(yardRehandle, fc, "Unit moved from Undeliverable to Deliverable location", ArgoUtils.timeNow())
+                }
             }
         }
     }
@@ -77,5 +98,5 @@ class ITSRecordYardRehandleGeneralNotice extends AbstractGeneralNoticeCodeExtens
         return false
     }
 
-    private static final Logger log  = Logger.getLogger(this.class)
+    private static final Logger log = Logger.getLogger(this.class)
 }
