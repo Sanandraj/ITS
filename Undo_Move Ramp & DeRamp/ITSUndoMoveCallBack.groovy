44a719a4becf911d020e712a1912674aa1ba99f8
HarikumarMohan <74703806+HarikumarMohan@users.noreply.github.com>
2022-11-21 16:12:14 +0530

    Remove event & chs validation changes

--- Undo_Move Ramp & DeRamp/ITSUndoMoveCallBack.groovy
+++ Undo_Move Ramp & DeRamp/ITSUndoMoveCallBack.groovy
@@ -4,15 +4,22 @@ import com.navis.argo.business.atoms.EventEnum
 import com.navis.argo.business.atoms.LocTypeEnum
 import com.navis.argo.business.atoms.WiMoveKindEnum
 import com.navis.argo.business.model.LocPosition
+import com.navis.argo.business.reference.Chassis
 import com.navis.external.framework.persistence.AbstractExtensionPersistenceCallback
+import com.navis.framework.business.Roastery
 import com.navis.framework.persistence.HibernateApi
+import com.navis.inventory.business.api.RectifyParms
+import com.navis.inventory.business.api.UnitFinder
 import com.navis.inventory.business.atoms.UfvTransitStateEnum
 import com.navis.inventory.business.atoms.UnitVisitStateEnum
 import com.navis.inventory.business.moves.MoveEvent
 import com.navis.inventory.business.units.MoveInfoBean
+import com.navis.inventory.business.units.Unit
 import com.navis.inventory.business.units.UnitFacilityVisit
+import com.navis.services.business.api.EventManager
 import com.navis.services.business.event.Event
 import com.navis.services.business.rules.EventType
+import org.apache.commons.lang.StringUtils
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
@@ -34,6 +41,8 @@ class ITSUndoMoveCallBack extends AbstractExtensionPersistenceCallback {
         int successCount = 0;
         String errorMessage = "";
         MoveEvent moveEvent = null
+        EventManager eventManager = (EventManager) Roastery.getBean(EventManager.BEAN_ID)
+
         if (inParams != null) {
             List<Serializable> inGkeys = (List<Serializable>) inParams.get("gkeys");
             try {
@@ -66,6 +75,7 @@ class ITSUndoMoveCallBack extends AbstractExtensionPersistenceCallback {
                                                         "[FAILED] " + ufv.getUfvUnit().getUnitId() + " Last move doesn't match the UNDO move")
                                             }
                                             CarrierVisitPhaseEnum ibVisitPhase = ufv?.getUfvActualIbCv()?.getCvVisitPhase()
+                                            CarrierVisitPhaseEnum obVisitPhase = ufv?.getUfvActualObCv()?.getCvVisitPhase()
                                             UfvTransitStateEnum transitState = null;
                                             UnitVisitStateEnum visitState = null;
                                             if (infoFromMoveEvent.getMoveKind() != null && infoFromMoveEvent.getMoveKind() != WiMoveKindEnum.Other) {
@@ -83,12 +93,12 @@ class ITSUndoMoveCallBack extends AbstractExtensionPersistenceCallback {
                                                         visitState = UnitVisitStateEnum.ADVISED
                                                         transitState = UfvTransitStateEnum.S10_ADVISED
                                                     }
-                                                    if (ibVisitPhase !=null){
-                                                    if (ibVisitPhase == CarrierVisitPhaseEnum.INBOUND ||
-                                                            [CarrierVisitPhaseEnum.ARRIVED, CarrierVisitPhaseEnum.WORKING].contains(ibVisitPhase)) {
-                                                        visitState = UnitVisitStateEnum.ACTIVE
-                                                        transitState = UfvTransitStateEnum.S20_INBOUND
-                                                    }
+                                                    if (ibVisitPhase != null) {
+                                                        if (ibVisitPhase == CarrierVisitPhaseEnum.INBOUND ||
+                                                                [CarrierVisitPhaseEnum.ARRIVED, CarrierVisitPhaseEnum.WORKING].contains(ibVisitPhase)) {
+                                                            visitState = UnitVisitStateEnum.ACTIVE
+                                                            transitState = UfvTransitStateEnum.S20_INBOUND
+                                                        }
                                                     }
 
                                                     ufv.setUfvVisitState(visitState)
@@ -97,14 +107,52 @@ class ITSUndoMoveCallBack extends AbstractExtensionPersistenceCallback {
                                                     if (ufv.ufvVisitState == UnitVisitStateEnum.ACTIVE) {
                                                         ufv.setUfvVisibleInSparcs(true)
                                                     }
-
+                                                    EventType unitRailEvnt = EventType.findEventType(EventEnum.UNIT_IN_RAIL.getKey());
+                                                    if (unitRailEvnt != null) {
+                                                        Event event = eventManager.getMostRecentEventByType(unitRailEvnt, ufv?.getUfvUnit());
+                                                        if (event != null) {
+                                                            event.purge();
+                                                        }
+                                                    }
+                                                }
+                                                if (!LocTypeEnum.YARD.equals(toPosition.getPosLocType()) && LocTypeEnum.YARD.equals(fromPosition.getPosLocType())) {
+                                                    if (obVisitPhase != null) {
+                                                        if ([CarrierVisitPhaseEnum.ARCHIVED, CarrierVisitPhaseEnum.CLOSED, CarrierVisitPhaseEnum.DEPARTED].contains(obVisitPhase)) {
+                                                            throw new Exception("[FAILED] " + moveKind + ": " + ufv.getUfvUnit().getUnitId() + " Carrier already departed")
+                                                        }
+                                                    }
                                                 }
                                                 if (toPosition.getPosLocType() != LocTypeEnum.YARD && fromPosition.getPosLocType() == LocTypeEnum.YARD) {
                                                     ufv.setUfvTransitState(UfvTransitStateEnum.S40_YARD)
                                                     ufv.setUfvVisitState(UnitVisitStateEnum.ACTIVE)
                                                     ufv.setUfvTimeOut(null)
                                                     ufv.setUfvVisibleInSparcs(true)
-                                                    ufv.getUfvUnit().setUnitVisitState(UnitVisitStateEnum.ACTIVE);
+                                                    EventType unitDismountEvnt = EventType.findEventType(EventEnum.UNIT_DISMOUNT.getKey());
+                                                    if (unitDismountEvnt != null) {
+                                                        Event event = eventManager.getMostRecentEventByType(unitDismountEvnt, ufv.getUfvUnit());
+                                                        if (event != null) {
+                                                            String notes = event?.getEventNote() != null ? event.getEventNote() : null
+                                                            if (notes != null) {
+                                                                String no = StringUtils.substringBefore(notes, 'dismounted')
+                                                                Chassis chassis = Chassis.findChassis(no?.trim())
+                                                                if (chassis != null) {
+                                                                    Unit unit = unitFinder.findActiveUnit(ContextHelper.getThreadComplex(), chassis)
+                                                                    if (unit != null) {
+                                                                        UnitFacilityVisit chassisUfv = unit?.getUnitActiveUfvNowActive()
+                                                                        if (chassisUfv != null && UfvTransitStateEnum.S40_YARD.equals(chassisUfv.getUfvTransitState())) {
+                                                                            try {
+                                                                                ufv?.getUfvUnit()?.attachCarriage(unit?.getUnitEquipment())
+                                                                                //event.purge();
+                                                                            } catch (Exception e) {
+                                                                                LOGGER.debug("error while attaching chassis to unit" + e)
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+
                                                     HibernateApi.getInstance().save(ufv.getUfvUnit());
                                                 }
                                                 infoFromMoveEvent.setMoveKind(WiMoveKindEnum.Other)
@@ -140,4 +188,7 @@ class ITSUndoMoveCallBack extends AbstractExtensionPersistenceCallback {
         inOutResults.put("ErrorMsg", errorMessage);
         inOutResults.put("Success", successCount);
     }
+
+    UnitFinder unitFinder = (UnitFinder) Roastery.getBean(UnitFinder.BEAN_ID);
+
 }
