bb78fe9195257de878f2033609fe71c8a6eac83c
KGOPINATH85 <32162554+KGOPINATH85@users.noreply.github.com>
2022-11-24 23:31:48 +0530

    ITSVesselReadyToBillGenNotice -- Updating as per the requirement shared by Anita
    ITSVesselReadyToBillGenNotice -- Updating as per the requirement shared by Anita

--- ITSVesselReadyToBillGenNotice.groovy
+++ ITSVesselReadyToBillGenNotice.groovy
@@ -3,17 +3,26 @@
  *
  */
 
+import com.navis.argo.ContextHelper
 import com.navis.argo.business.api.IEventType
 import com.navis.argo.business.api.ServicesManager
+import com.navis.argo.business.atoms.CarrierVisitPhaseEnum
 import com.navis.argo.business.atoms.CarrierVisitReadyToBillEnum
-import com.navis.argo.business.model.GeneralReference
+import com.navis.argo.business.extract.ChargeableMarineEvent
+import com.navis.argo.business.reference.ScopedBizUnit
+import com.navis.argo.webservice.types.v1_0.GenericInvokeResponseWsType
+import com.navis.argo.webservice.types.v1_0.ResponseType
+import com.navis.argo.webservice.types.v1_0.ScopeCoordinateIdsWsType
+import com.navis.external.framework.util.ExtensionUtils
 import com.navis.external.services.AbstractGeneralNoticeCodeExtension
 import com.navis.framework.business.Roastery
 import com.navis.framework.metafields.MetafieldIdFactory
+import com.navis.services.business.api.EventManager
 import com.navis.services.business.event.Event
 import com.navis.services.business.event.EventFieldChange
 import com.navis.services.business.event.GroovyEvent
 import com.navis.vessel.business.schedule.VesselVisitDetails
+import com.navis.www.services.argoservice.ArgoServicePort
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
@@ -47,83 +56,100 @@ class ITSVesselReadyToBillGenNotice extends AbstractGeneralNoticeCodeExtension {
     @Override
     void execute(GroovyEvent inGroovyEvent) {
         LOGGER.setLevel(Level.DEBUG)
-        LOGGER.debug("ITSVesselReadyToBillGenNotice started")
-        VesselVisitDetails vvDetail = inGroovyEvent != null ? (VesselVisitDetails) inGroovyEvent.getEntity() : null;
-        EventFieldChange eventFieldChange = inGroovyEvent != null ? getFieldChange(inGroovyEvent.getEvent(), MetafieldIdFactory.valueOf("cvdCv.cvReadyToInvoice").getFieldId()) : null;
-
+        VesselVisitDetails vvDetail = inGroovyEvent != null ? (VesselVisitDetails) inGroovyEvent.getEntity() : null
+        Object library = ExtensionUtils.getLibrary(ContextHelper.getThreadUserContext(), "ITSAutoBillMarineUtility")
+        EventFieldChange eventFieldChange = inGroovyEvent != null ? library.getFieldChange(inGroovyEvent.getEvent(), MetafieldIdFactory.valueOf("cvdCv.cvReadyToInvoice").getFieldId()) : null
         if (eventFieldChange != null && vvDetail != null && vvDetail.getCvdCv() != null) {
-            LOGGER.debug("ITSVesselReadyToBillGenNotice vvDetail state : " + vvDetail.getCvdCv().getCvReadyToInvoice())
-            if (CarrierVisitReadyToBillEnum.READY.equals(vvDetail.getCvdCv().getCvReadyToInvoice())) {
-                IEventType inEventType = null;
-                ServicesManager srvcMgr = (ServicesManager) Roastery.getBean(ServicesManager.BEAN_ID);
-                List<String> eventToRecord = getEventsToRecord();
-                if (eventToRecord != null && !eventToRecord.isEmpty()) {
-                    for (String event : eventToRecord) {
-                        try {
-                            if (event != null) {
-                                inEventType = srvcMgr.getEventType(event);
-                                LOGGER.debug("ITSVesselReadyToBillGenNotice inEvent " + inEventType)
-                                if (inEventType != null && vvDetail != null) {
-                                    srvcMgr.recordEvent(inEventType, null, null, null, vvDetail)
+            if (CarrierVisitPhaseEnum.COMPLETE.equals(vvDetail.getVvdVisitPhase()) || CarrierVisitPhaseEnum.DEPARTED.equals(vvDetail.getVvdVisitPhase())) {
+                ScopedBizUnit vvOperator = vvDetail.getCvdCv() != null ? vvDetail.getCvdCv().getCvOperator() : null
+                String lineId = vvOperator != null ? vvOperator.getBzuId() : null
+                List<String> eventToRecord = library.getEventsToRecord(lineId)
+                ServicesManager srvcMgr = (ServicesManager) Roastery.getBean(ServicesManager.BEAN_ID)
+                EventManager eventManager = (EventManager) Roastery.getBean("eventManager")
+                IEventType inEventType = null
+                if (CarrierVisitReadyToBillEnum.READY.equals(vvDetail.getCvdCv().getCvReadyToInvoice())) {
+                    if (eventToRecord != null && !eventToRecord.isEmpty()) {
+                        for (String event : eventToRecord) {
+                            try {
+                                if (event != null) {
+                                    inEventType = srvcMgr.getEventType(event)
+                                    if (inEventType != null && vvDetail != null) {
+                                        srvcMgr.recordEvent(inEventType, null, null, null, vvDetail)
+                                    }
                                 }
-
+                            } catch (Exception ex) {
+                                LOGGER.error("ITSVesselReadyToBillGenNotice error while recording an event " + ex)
                             }
-                        } catch (Exception ex) {
-                            LOGGER.debug("ITSVesselReadyToBillGenNotice error while recording an event " + ex)
+
                         }
-
                     }
+                } else {
+                    if (eventToRecord != null && !eventToRecord.isEmpty()) {
+                        Event cancelEvent = null
+                        Long extractBatchId = null
+                        List<ChargeableMarineEvent> marineEventList = null
+                        for (String event : eventToRecord) {
+                            cancelEvent = null
+                            try {
+                                if (event != null) {
+                                    inEventType = srvcMgr.getEventType(event)
+                                    cancelEvent = eventManager.getMostRecentEventByType(inEventType, vvDetail)
+                                    if (cancelEvent != null) {
+                                        extractBatchId = cancelEvent.getEvntBillingExtractBatchId()
+                                        if (extractBatchId != null) {
+                                            marineEventList = library.findCME(extractBatchId)
+                                            if (marineEventList != null) {
+                                                for (ChargeableMarineEvent inCME : marineEventList) {
+                                                    if (inCME != null) {
+                                                        if ("QUEUED".equals(inCME.getStatus())) {
+                                                            inCME.setStatusCancelled()
+                                                        } else if ("DRAFT".equals(inCME.getStatus())) {
+                                                            if (inCME != null) {
+                                                                String draftNbr = inCME.getLastDraftInvNbr()
+                                                                String deleteInvoiceXML = draftNbr != null ? library.deleteInvoiceRequest(draftNbr) : null
+                                                                ScopeCoordinateIdsWsType scopeCoordinates = library.getScopeCoordinatesForWebService()
+                                                                ArgoServicePort servicePort = library.getWebServiceStub()
+                                                                if (servicePort != null && deleteInvoiceXML != null) {
+                                                                    GenericInvokeResponseWsType webServiceResponse = scopeCoordinates != null ? servicePort.genericInvoke(scopeCoordinates, deleteInvoiceXML) : null
+                                                                    ResponseType ptResponse = webServiceResponse != null ? webServiceResponse.getCommonResponse() : null
+                                                                    if (ptResponse == null) {
+                                                                        LOGGER.error("Something went wrong in N4 Billing.Billing invoice request failed")
+                                                                    } else {
+                                                                        LOGGER.debug("ITSAutoGenerateMarineInvoiceGenNotice ptResponse :" + ptResponse)
+                                                                    }
+                                                                }
+
+                                                            }
+                                                            inCME.setStatusCancelled()
+
+                                                        } else if ("INVOICED".equals(inCME.getStatus())) {
+                                                            // do nothing here
+                                                            LOGGER.debug("CME is INVOICED, not able to delete it " + inCME.getStatus())
+                                                        } else {
+                                                            // do nothing here
+                                                            LOGGER.debug("CME not in proper status " + inCME.getStatus())
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        //cancelEvent.setEventTypeIsBillable(false);
+                                    }
+
+                                }
+                            } catch (Exception ex) {
+                                LOGGER.debug("ITSVesselReadyToBillGenNotice error while recording an event " + ex)
+                            }
+
+                        }
+                    }
+
                 }
+
             }
+
         }
-    }
-/**
- *
- * @param inEvent
- * @param inMetafieldId
- * @return
- */
-    private static EventFieldChange getFieldChange(Event inEvent, String inMetafieldId) {
-        Set<EventFieldChange> fcs = inEvent != null ? inEvent.getFieldChanges() : null
-        if (fcs != null && inMetafieldId != null) {
-            for (EventFieldChange efc : fcs) {
-                if (inMetafieldId.equals(efc.getEvntfcMetafieldId())) {
-                    return efc
-                }
-            }
-        }
-        return null
     }
 
-/**
- * Get the Genereal Reference value
- * @return
- */
-    private static List<String> getEventsToRecord() {
-        GeneralReference inEvntFromGenRef = GeneralReference.findUniqueEntryById("ITS", "VESSEL_READY_TO_BILL", "BILLABLE_EVENT", null)
-        List<String> inEvntList = new ArrayList<String>()
-
-        if (inEvntFromGenRef != null) {
-            if (inEvntFromGenRef.getRefValue1() != null) {
-                inEvntList.addAll(inEvntFromGenRef.getRefValue1().split(","))
-            }
-            if (inEvntFromGenRef.getRefValue2() != null) {
-                inEvntList.addAll(inEvntFromGenRef.getRefValue2().split(","))
-            }
-            if (inEvntFromGenRef.getRefValue3() != null) {
-                inEvntList.addAll(inEvntFromGenRef.getRefValue3().split(","))
-            }
-            if (inEvntFromGenRef.getRefValue4() != null) {
-                inEvntList.addAll(inEvntFromGenRef.getRefValue4().split(","))
-            }
-            if (inEvntFromGenRef.getRefValue5() != null) {
-                inEvntList.addAll(inEvntFromGenRef.getRefValue5().split(","))
-            }
-            if (inEvntFromGenRef.getRefValue6() != null) {
-                inEvntList.addAll(inEvntFromGenRef.getRefValue6().split(","))
-            }
-        }
-        return inEvntList != null ? inEvntList : null
-    }
 
 }
