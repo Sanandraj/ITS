c852063ee55d54182bbb04cf9c3ad515fe938c2a
MADHAVAN M <118721016+MADHAVAN1503@users.noreply.github.com>
2022-12-16 20:01:09 +0530

    Update ITSDeliverWheeledEmptiesOnPriority.groovy

--- ITSDeliverWheeledEmptiesOnPriority.groovy
+++ ITSDeliverWheeledEmptiesOnPriority.groovy
@@ -51,12 +51,10 @@ class ITSDeliverWheeledEmptiesOnPriority extends AbstractGateTaskInterceptor {
         if (inTran == null) {
             return;
         }
-        //Pool.findOrCreatePool()
-
         ScopedBizUnit inLinOp = inTran.getTranAppointment()?.getGapptLineOperator()
         EquipmentOrderItem odrItm = inTran.getTranEqoItem()
         if (inLinOp != null && odrItm != null) {
-            if (inTran.getTranCtrNbrAssigned() == null && inTran.getTranChsNbr() == null) {  // bobtails
+            if (inTran.getTranCtrNbrAssigned() == null && inTran.getTranChsNbr() == null) {            // bobtails
                 Unit inUnit = findWheeledUnitsPriority(inLinOp, odrItm, true, false)
                 LOGGER.debug("inUnit ::" + inUnit)
                 if (inUnit != null) {
@@ -71,7 +69,7 @@ class ITSDeliverWheeledEmptiesOnPriority extends AbstractGateTaskInterceptor {
                     String errorMsg = "Suitable Wheeled/Grounded Containers Not Found";
                     RoadBizUtil.messageCollector.appendMessage(MessageLevel.SEVERE, AllOtherFrameworkPropertyKeys.ERROR__NULL_MESSAGE, null, errorMsg);
                 }
-            } else if (inTran.getTranCtrNbrAssigned() == null) {        //chassis
+            } else if (inTran.getTranCtrNbrAssigned() == null) {               //chassis
                 Unit inUnit = findWheeledUnitsPriority(inLinOp, odrItm, false, true)
                 LOGGER.debug("inUnit Grounded::" + inUnit)
                 if (inUnit != null) {
@@ -89,75 +87,63 @@ class ITSDeliverWheeledEmptiesOnPriority extends AbstractGateTaskInterceptor {
 
     private Unit findWheeledUnitsPriority(ScopedBizUnit lineOp, EquipmentOrderItem OrdrItm, Boolean Wheeled, Boolean Grounded) {
 
-        List<Serializable> lineList = new ArrayList<>()
-        lineList.add(lineOp.getPrimaryKey())
-        LOGGER.debug("linelist1::" + lineList)
-
+        List<Serializable> memberLineList = new ArrayList<>()
+        memberLineList.add(lineOp.getPrimaryKey())
+         /*start equipment pools validation*/
         DomainQuery df = QueryUtils.createDomainQuery(PoolsEntity.POOL)
                 .addDqPredicate(PredicateFactory.eq(PoolField.POOL_ADMIN_LIFE_CYCLE_STATE, LifeCycleStateEnum.ACTIVE))
                 .addDqOrdering(Ordering.asc(PoolField.POOL_NAME))
         List<Pool> listPools = Roastery.getHibernateApi().findEntitiesByDomainQuery(df);
-        LOGGER.debug("listpools::" + listPools)
-        List<ScopedBizUnit> agrmtMembers = new ArrayList<>()
-        List<ScopedBizUnit> tempMem=new ArrayList<>();
+        List<ScopedBizUnit> equPoolFinalMembers = new ArrayList<>()
+        List<ScopedBizUnit> tempMembers=new ArrayList<>();
         for (Pool pool : listPools) {
-            boolean contains= false;
-            tempMem.clear();
-            LOGGER.debug("tempMem::" +tempMem)
+            boolean eqmLineFind= false
+            tempMembers.clear()
             Set poolMembers = pool.getPoolMembers()
-            LOGGER.debug("poolmembers::" + poolMembers)
             for (PoolMember member : poolMembers) {
                 ScopedBizUnit eqOperator = (ScopedBizUnit) member.getPoolmbrEqOper();
-                LOGGER.debug("eqOperator::" + eqOperator)
                 if (member.getLifeCycleState() == LifeCycleStateEnum.ACTIVE && eqOperator.equals(lineOp)) {
-                    LOGGER.debug("true.....")
-                    contains=true
+                    eqmLineFind=true
                 }
                 if (eqOperator != null) {
-                    tempMem.add(eqOperator)
-                    LOGGER.debug("tempMem::" +tempMem)
+                    tempMembers.add(eqOperator)
                 }
             }
-            if (contains){
-                for(ScopedBizUnit tt:tempMem){
-                    agrmtMembers.add(tt)
+            if (eqmLineFind){
+                for(ScopedBizUnit memberLists:tempMembers){
+                    equPoolFinalMembers.add(memberLists)
                 }
+            }
+        }
+        /*End EquipmentPools validation*/
 
-                LOGGER.debug("agrmtMembers::" +agrmtMembers)
-            }
+        for(ScopedBizUnit scopePrimaryKey:equPoolFinalMembers){
+            memberLineList.add(scopePrimaryKey.getPrimaryKey())
         }
-        for(ScopedBizUnit inLine2:agrmtMembers){
-            lineList.add(inLine2.getPrimaryKey())
-        }
-        LOGGER.debug("linelist::" + lineList)
+
         DomainQuery dq = QueryUtils.createDomainQuery("Unit")
                 .addDqPredicate(PredicateFactory.eq(UnitField.UNIT_CATEGORY, UnitCategoryEnum.STORAGE))
-                .addDqPredicate(PredicateFactory.in(UnitField.UNIT_LINE_OPERATOR, lineList))
+                .addDqPredicate(PredicateFactory.in(UnitField.UNIT_LINE_OPERATOR, memberLineList))
                 .addDqPredicate(PredicateFactory.eq(UnitField.UNIT_EQ_ISO_GROUP, OrdrItm.getEqoiEqIsoGroup()))
                 .addDqPredicate(PredicateFactory.eq(UnitField.UNIT_EQ_NOMINAL_LENGTH, OrdrItm.getEqoiEqSize()))
                 .addDqPredicate(PredicateFactory.eq(UnitField.UNIT_EQTYP_HEIGHT, OrdrItm.getEqoiEqHeight()))
                 .addDqPredicate(PredicateFactory.eq(UnitField.UNIT_VISIT_STATE, UnitVisitStateEnum.ACTIVE))
                 .addDqPredicate(PredicateFactory.eq(UnitField.UNIT_CURRENT_UFV_TRANSIT_STATE, UfvTransitStateEnum.S40_YARD))
-        List<ScopedBizUnit> scopList = new ArrayList<>()
-        for(Serializable serializable : lineList){
+        List<ScopedBizUnit> listScopedBizUnit = new ArrayList<>()
+        for(Serializable serializable : memberLineList){
             ScopedBizUnit scopedBizUnit = ScopedBizUnit.hydrate(serializable)
-            scopList.add(scopedBizUnit)
+            listScopedBizUnit.add(scopedBizUnit)
         }
-        LOGGER.debug("scopList::" + scopList)
-
         List<Unit> units = HibernateApi.getInstance().findEntitiesByDomainQuery(dq)
-        LOGGER.debug("units ::" + units.toString())
         if (units != null && units.size() > 0) {
-            for (ScopedBizUnit line : (scopList as List<ScopedBizUnit>)) {
+            for (ScopedBizUnit line : (listScopedBizUnit as List<ScopedBizUnit>)) {
                 if (Wheeled) {
                     LOGGER.debug("Inside Wheeled ::" + Wheeled)
                     for (Unit unit : (units as List<Unit>)) {
                         if (unit != null && unit.getUnitLineOperator().equals(line)) {
-                            LOGGER.debug("unit ::" + unit.getUnitId())
                             UnitFacilityVisit ufv = unit.getUnitActiveUfvNowActive()
                             if (ufv != null) {
                                 List unitDeli = ufv.getWorkInstsWithMoveKind(WiMoveKindEnum.Delivery)
-                                LOGGER.debug("unitDeli ::" + unitDeli)
                                 if ((unitDeli == null || unitDeli.isEmpty()) && unit.findCurrentPosition()?.isWheeled() && unit.getUnitCurrentlyAttachedChassisId() != null) {
                                     StackStatus stackStatus = StackStatus.findStackStatus(ufv.getUfvLastKnownPosition()?.getPosBin(), ContextHelper.getThreadYard())
                                     String deliverable = stackStatus != null ? StackStatusUtils.getProtectedStatus(stackStatus.getStackstatusStatusChars()) : null
@@ -173,17 +159,14 @@ class ITSDeliverWheeledEmptiesOnPriority extends AbstractGateTaskInterceptor {
                         }
                     }
                     Grounded = true
-                    LOGGER.debug("Grounded ::" + Grounded)
                 }
                 if (Grounded) {
                     LOGGER.debug("Inside Grounded ::" + Grounded)
                     for (Unit unit : (units as List<Unit>)) {
                         if (unit != null && unit.getUnitLineOperator().equals(line)) {
-                            LOGGER.debug("unit ::" + unit.getUnitId())
                             UnitFacilityVisit ufv = unit.getUnitActiveUfvNowActive()
                             if (ufv != null) {
                                 List unitDeli = ufv.getWorkInstsWithMoveKind(WiMoveKindEnum.Delivery)
-                                LOGGER.debug("unitDeli ::" + unitDeli)
                                 if ((unitDeli == null || unitDeli.isEmpty()) && unit.getUnitCurrentlyAttachedChassisId() == null) {
                                     StackStatus stackStatus = StackStatus.findStackStatus(ufv.getUfvLastKnownPosition()?.getPosBin(), ContextHelper.getThreadYard())
                                     String deliverable = stackStatus != null ? StackStatusUtils.getProtectedStatus(stackStatus.getStackstatusStatusChars()) : null
