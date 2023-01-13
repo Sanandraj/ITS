c01491fdf25ea0da3ca0c2b1a204c096a8457b6f
AshokkumarKD <104904543+AshokkumarKD@users.noreply.github.com>
2023-01-13 13:51:02 +0530

    IP-273 final review done

--- Enhancements/customBeanYardBlockTableManager.groovy
+++ Enhancements/customBeanYardBlockTableManager.groovy
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022 WeServe LLC. All Rights Reserved.
+ *
+*/
+
 import com.navis.argo.ArgoEntity
 import com.navis.argo.ArgoField
 import com.navis.external.framework.beans.EBean
@@ -7,11 +12,26 @@ import com.navis.framework.portal.query.PredicateFactory
 import com.navis.framework.presentation.view.DefaultSharedUiTableManager
 
 /**
- * uaarthi@weservetech.com 09/05/2022
- * Custom Table manager UI view to View/update Yard blocks as Deliverable/ Non deliverable, General Reference Key - DELIVERABLE_BLOCK
+ * @Author: mailto:uaarthi@weservetech.com, Aarthi U; Date: 11/15/2022
  *
- * uaarthi@weservetech.com 11/15/2022
- * TT-37 Yard Area Close - Updated groovy to provide Custom view to include Bay as Deliverable/ Non deliverable, General Reference Key - DELIVERABLE_BAY
+ *  Requirements: Custom Table manager UI view to View/update Yard blocks as Deliverable/ Non deliverable, General Reference Key - DELIVERABLE_BLOCK
+ *                TT-37 Yard Area Close - Updated groovy to provide Custom view to include Bay as Deliverable/ Non deliverable, General Reference Key - DELIVERABLE_BAY
+ *
+ * @Inclusion Location: Incorporated as a code extension of the type
+ *
+ *  Load Code Extension to N4:
+ *  1. Go to Administration --> System --> Code Extensions
+ *  2. Click Add (+)
+ *  3. Enter the values as below:
+ *     Code Extension Name: customBeanYardBlockTableManager
+ *     Code Extension Type:  BEAN_PROTOTYPE
+ *     Groovy Code: Copy and paste the contents of groovy code.
+ *  4. Click Save button
+ *
+ * @Setup Create the code Extension customBeanYardBlockTableManager in Global level
+ *
+ *
+ *  S.No    Modified Date   Modified By     Jira      Description
  *
  */
 class customBeanYardBlockTableManager extends DefaultSharedUiTableManager implements EBean {
@@ -23,6 +43,6 @@ class customBeanYardBlockTableManager extends DefaultSharedUiTableManager implem
     @Override
     DataQuery createQuery() {
         return QueryUtils.createDomainQuery(ArgoEntity.GENERAL_REFERENCE)
-                .addDqPredicate(PredicateFactory.in(ArgoField.REF_ID1, "DELIVERABLE_BLOCK","DELIVERABLE_BAY"))
+                .addDqPredicate(PredicateFactory.in(ArgoField.REF_ID1, "DELIVERABLE_BLOCK", "DELIVERABLE_BAY"))
     }
 }
