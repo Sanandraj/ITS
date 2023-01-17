50f789fc8d13b7ffe7919041c9ad223407bd2f8a
AshokkumarKD <104904543+AshokkumarKD@users.noreply.github.com>
2023-01-17 18:59:26 +0530

    IP-294 final review done
    Updated header

--- Service Order/ITSServiceOrderBeanSeeder.groovy
+++ Service Order/ITSServiceOrderBeanSeeder.groovy
@@ -3,23 +3,26 @@
 import com.navis.external.framework.beans.ui.AbstractCustomUiBeanSeeder
 
 /**
- * @Author: uaarthi@weservetech.com; Date: 21-12-2022
+ * @Author: mailto:uaarthi@weservetech.com, Aarthi U; Date: 21/12/2022
  *
  *  Requirements: To seed the Service related Beans into N4
- *  customBeanITSShowCreateServiceOrderFormController
+ *                customBeanITSShowCreateServiceOrderFormController
  *
- * @Inclusion Location: Incorporated as a code extension of the type
+ *  @Inclusion Location: Incorporated as a code extension of the type
  *
  *  Load Code Extension to N4:
  *  1. Go to Administration --> System --> Code Extensions
  *  2. Click Add (+)
  *  3. Enter the values as below:
  *     Code Extension Name: ITSServiceOrderBeanSeeder
- *     Code Extension Type:  SERVER_UI_TIER_LIFECYCLE
+ *     Code Extension Type: SERVER_UI_TIER_LIFECYCLE
  *     Groovy Code: Copy and paste the contents of groovy code.
  *  4. Click Save button
  *
+ *	@Setup Create the code extension ITSServiceOrderBeanSeeder with extension type SERVER_UI_TIER_LIFECYCLE
  *
+ *
+ *  S.No    Modified Date   Modified By     Jira      Description
  *
  */
 class ITSServiceOrderBeanSeeder extends AbstractCustomUiBeanSeeder{
