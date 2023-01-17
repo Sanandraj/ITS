ddee9963a897ede24ae9f3bda0918210399f4dd1
AshokkumarKD <104904543+AshokkumarKD@users.noreply.github.com>
2023-01-17 16:41:02 +0530

    IP-409 final review done
    Header updated.
    Description: Multiple wavier and Guarantee

--- ITSGuaranteeAndWaiverBeanSeeder.groovy
+++ ITSGuaranteeAndWaiverBeanSeeder.groovy
@@ -6,30 +6,27 @@
 import com.navis.external.framework.beans.ui.AbstractCustomUiBeanSeeder
 
 /*
-     *
-     * @Author : Gopinath Kannappan, 29/Dec/2022
-     *
-     * Requirements : 7-9 Apply Waiver to Multiple Units -- This groovy is used to load these controller - customBeanITSRecordGuaranteeFormController and customBeanITSRecordWaiverFormController.
-     *
-     * @Inclusion Location	: Incorporated as a code extension of the type SERVER_UI_TIER_LIFE_CYCLE.
-     *
-     *  Load Code Extension to N4:
-            1. Go to Administration --> System -->  Code Extension
-            2. Click Add (+)
-            3. Enter the values as below:
-                Code Extension Name:  ITSGuaranteeAndWaiverBeanSeeder
-                Code Extension Type:  SERVER_UI_TIER_LIFE_CYCLE
-               Groovy Code: Copy and paste the contents of groovy code.
-            4. Click Save button
-
-     *  Set up  />
-     *
-     *
+ * @Author: mailto:kgopinath@weservetech.com, Gopinath Kannappan; Date: 29/12/2022
+ *
+ *  Requirements: 7-9 Apply Waiver to Multiple Units -- This groovy is used to load these controller - customBeanITSRecordGuaranteeFormController and customBeanITSRecordWaiverFormController.
+ *
+ *  @Inclusion Location: Incorporated as a code extension of the type
+ *
+ *  Load Code Extension to N4:
+ *  1. Go to Administration --> System --> Code Extensions
+ *  2. Click Add (+)
+ *  3. Enter the values as below:
+ *     Code Extension Name: ITSGuaranteeAndWaiverBeanSeeder
+ *     Code Extension Type: SERVER_UI_TIER_LIFE_CYCLE
+ *     Groovy Code: Copy and paste the contents of groovy code.
+ *  4. Click Save button
+ *
+ *  S.No    Modified Date   Modified By     Jira      Description
+ *
  */
 
 
 class ITSGuaranteeAndWaiverBeanSeeder extends AbstractCustomUiBeanSeeder {
-
 
 
     protected String[] getBeanNames() {
