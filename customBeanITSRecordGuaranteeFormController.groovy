3a2634d1ce8e5b454c04791c96a1496400659f04
AshokkumarKD <104904543+AshokkumarKD@users.noreply.github.com>
2023-01-17 16:20:47 +0530

    IP-409 Final review done
    Removed Logs and updated header

--- customBeanITSRecordGuaranteeFormController.groovy
+++ customBeanITSRecordGuaranteeFormController.groovy
@@ -13,25 +13,25 @@ import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
 /*
-     *
-     * @Author : Gopinath Kannappan, 29/Dec/2022
-     *
-     * Requirements : 7-9 Apply Waiver to Multiple Units -- This groovy is used to record the guarantee for multiple units.
-     *
-     * @Inclusion Location	: Incorporated as a code extension of the type BEAN_PROTOTYPE.
-     *
-     *  Load Code Extension to N4:
-            1. Go to Administration --> System -->  Code Extension
-            2. Click Add (+)
-            3. Enter the values as below:
-                Code Extension Name:  customBeanITSRecordGuaranteeFormController
-                Code Extension Type:  BEAN_PROTOTYPE
-               Groovy Code: Copy and paste the contents of groovy code.
-            4. Click Save button
-
-     *  Set up in Guarantee form as : <controller ref="customBeanITSRecordGuaranteeFormController" />
-     *
-     *
+ * @Author: mailto:kgopinath@weservetech.com, Gopinath Kannappan; Date: 29/12/2022
+ *
+ *  Requirements: 7-9 Apply Waiver to Multiple Units -- This groovy is used to record the guarantee for multiple units.
+ *
+ *  @Inclusion Location: Incorporated as a code extension of the type
+ *
+ *  Load Code Extension to N4:
+ *  1. Go to Administration --> System --> Code Extensions
+ *  2. Click Add (+)
+ *  3. Enter the values as below:
+ *     Code Extension Name: customBeanITSRecordGuaranteeFormController
+ *     Code Extension Type: BEAN_PROTOTYPE
+ *     Groovy Code: Copy and paste the contents of groovy code.
+ *  4. Click Save button
+ *
+ *	@Setup in Guarantee form as : <controller ref="customBeanITSRecordGuaranteeFormController" />
+ *
+ *  S.No    Modified Date   Modified By     Jira      Description
+ *
  */
 
 
@@ -59,7 +59,6 @@ class customBeanITSRecordGuaranteeFormController extends RecordGuaranteeFormCont
         Serializable gkey = null
         Serializable[] primaryKeys = null
         ICarinaLovWidget eventTypeWidget = (ICarinaLovWidget) this.getFormWidget(InventoryBizMetafield.EXTRACT_EVENT_TYPE)
-        LOGGER.debug("eventTypeWidget :" + eventTypeWidget)
         if (null != eventTypeWidget && eventTypeWidget instanceof ICarinaLovWidget) {
             if (null == this.getEntityGkey()) {
                 Object parent = this.getAttribute("parent")
@@ -74,8 +73,6 @@ class customBeanITSRecordGuaranteeFormController extends RecordGuaranteeFormCont
 
             ParentDependentLovKey parentDependentLovKey = new ParentDependentLovKey("argoExtractLov.ufvUnitEvent", primaryKeys)
             eventTypeWidget.setLovKey(parentDependentLovKey)
-
         }
-
     }
 }
