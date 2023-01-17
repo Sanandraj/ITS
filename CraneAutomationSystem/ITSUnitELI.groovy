b76b9a6d541291ab42e13a5f00f4f9e9908f13f9
Sanandraj <96609211+Sanandraj@users.noreply.github.com>
2023-01-17 14:56:31 +0530

    Final Review-IP-407

--- CraneAutomationSystem/ITSUnitELI.groovy
+++ CraneAutomationSystem/ITSUnitELI.groovy
@@ -1,28 +1,112 @@
-package CraneAutomationSystem
+/*
+ * Copyright (c) 2022 WeServe LLC. All Rights Reserved.
+ *
+ */
 
-import com.navis.argo.business.atoms.LocTypeEnum
+
+import com.navis.argo.business.api.ServicesManager
+import com.navis.argo.business.atoms.EquipClassEnum
+import com.navis.argo.business.model.LocPosition
+import com.navis.argo.business.reference.Equipment
 import com.navis.external.framework.entity.AbstractEntityLifecycleInterceptor
 import com.navis.external.framework.entity.EEntityView
 import com.navis.external.framework.util.EFieldChanges
 import com.navis.external.framework.util.EFieldChangesView
+import com.navis.framework.business.Roastery
+import com.navis.framework.portal.FieldChanges
 import com.navis.inventory.InvField
-import com.navis.inventory.business.atoms.UfvTransitStateEnum
 import com.navis.inventory.business.units.Unit
 import com.navis.inventory.business.units.UnitFacilityVisit
+import com.navis.services.business.rules.EventType
+import org.apache.commons.lang.StringUtils
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.*
 
 /**
- * @author <a href="mailto:sramasamy@weservetech.com"> Ramasamy Sathappan</a>
- * @since 13-May-2022
- * For CAS - OnBoardUnitUpdate
  *
- * */
+ * @Author <ahref="mailto:sramasamy@weservetech.com"   >   Ramasamy Sathappan</a>, 13/MAY/2022
+ *
+ * Requirements :For CAS - OnBoardUnitUpdate
+ *
+ * @Inclusion Location    : Incorporated as a code extension of the type ENTITY_LIFECYCLE_INTERCEPTION
+ *
+ *  Load Code Extension to N4:
+ 1. Go to Administration --> System -->  Code Extension
+ 2. Click Add (+)
+ 3. Enter the values as below:
+ Code Extension Name:  ITSUnitELI
+ Code Extension Type:  ENTITY_LIFECYCLE_INTERCEPTION
+ Groovy Code: Copy and paste the contents of groovy code.
+ 4. Click Save button
+ *
+ *  S.No      Modified Date        Modified By                                              Jira      Description
+ *   1        25/OCT/2022          mailto:annalakshmig@weservetech.com AnnaLakshmi G        IP-14     update the flex field (dwell date) whenever there is change in First Deliverable Date/Line LFD
+ */
+
 class ITSUnitELI extends AbstractEntityLifecycleInterceptor {
 
     @Override
     void onUpdate(EEntityView inEntity, EFieldChangesView inOriginalFieldChanges, EFieldChanges inMoreFieldChanges) {
         LOGGER.setLevel(Level.DEBUG);
+        Unit unit
+        UnitFacilityVisit unitFacilityVisit
+        if (inEntity._entity instanceof Unit) {
+            unit = (Unit) inEntity._entity;
+        } else {
+            unitFacilityVisit = (UnitFacilityVisit) inEntity._entity;
+            unit = unitFacilityVisit.getUfvUnit();
+        }
+
+        LOGGER.debug("ITSUnitELI inOriginalFieldChanges" + inOriginalFieldChanges)
+        if (unit != null && unit.getUnitEquipment() != null && EquipClassEnum.CONTAINER.equals(unit.getUnitEquipment().getEqClass()) && inOriginalFieldChanges != null && inOriginalFieldChanges.hasFieldChange(InvField.UFV_LAST_KNOWN_POSITION)) {
+            LocPosition locPositionOld = (LocPosition) inOriginalFieldChanges.findFieldChange(InvField.UFV_LAST_KNOWN_POSITION).getPriorValue()
+            LocPosition locPositionNew = (LocPosition) inOriginalFieldChanges.findFieldChange(InvField.UFV_LAST_KNOWN_POSITION).getNewValue()
+            FieldChanges fieldChanges = new FieldChanges();
+            if (locPositionOld != null && locPositionNew != null) {
+                if (locPositionOld.isWheeled() && locPositionNew.isGrounded()) {
+
+                    EventType eventType = EventType.findEventType("DISMOUNT")
+
+                    fieldChanges.setFieldChange(InvField.UFV_LAST_KNOWN_POSITION, inOriginalFieldChanges.findFieldChange(InvField.UFV_LAST_KNOWN_POSITION).getPriorValue(),
+                            inOriginalFieldChanges.findFieldChange(InvField.UFV_LAST_KNOWN_POSITION).getNewValue())
+                    servicesManager.recordEvent(eventType, "Recorded by groovy", null, null, unit, fieldChanges)
+                } else if (locPositionOld.isGrounded() && locPositionNew.isWheeled()) {
+                    EventType eventType = EventType.findEventType("MOUNT")
+                    fieldChanges.setFieldChange(InvField.UFV_LAST_KNOWN_POSITION, inOriginalFieldChanges.findFieldChange(InvField.UFV_LAST_KNOWN_POSITION).getPriorValue(),
+                            inOriginalFieldChanges.findFieldChange(InvField.UFV_LAST_KNOWN_POSITION).getNewValue())
+                    Equipment equipment = null
+                    if (unit.getUnitCurrentlyAttachedChassisId() != null) {
+
+                        equipment = Equipment.findEquipment(unit.getUnitCurrentlyAttachedChassisId())
+                    }
+                    servicesManager.recordEvent(eventType, "Recorded by groovy", null, null, unit, fieldChanges, null, equipment)
+                }
+            }
+
+
+        }
+        // to update the flex field (dwell date) whenever there is change in First Deliverable Date/Line LFD
+        if (unitFacilityVisit != null && inOriginalFieldChanges != null && (inOriginalFieldChanges.hasFieldChange(InvField.UFV_FLEX_DATE01) || inOriginalFieldChanges.hasFieldChange(InvField.UFV_LINE_LAST_FREE_DAY))) {
+            Date date = null
+            // Date firstDeliverableDate = (String) inOriginalFieldChanges.findFieldChange(InvField.UFV_FLEX_DATE01).getNewValue()
+            String lineLFD = unitFacilityVisit.getUfvCalculatedLineStorageLastFreeDay()
+            if (!StringUtils.isEmpty(lineLFD)) {
+                date = getLfdDate(lineLFD)
+                if (date) {
+                    LocalDateTime lcDate = date.plus(1).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+                    ZonedDateTime zonedDateTime = lcDate.withHour(3).withMinute(0).withSecond(0).atZone(ZoneOffset.systemDefault());
+                    Instant instant = zonedDateTime.toInstant();
+                    date = Date.from(instant);
+                }
+            }
+            inMoreFieldChanges.setFieldChange(InvField.UFV_FLEX_DATE02, date)
+
+
+        }
 
         try {
             Map map = new HashMap();
@@ -34,37 +118,23 @@ class ITSUnitELI extends AbstractEntityLifecycleInterceptor {
             Object object = getLibrary(T__ON_BOARD_UNIT_UPDATE);
             object.execute(map);
 
-            /*if (inOriginalFieldChanges.hasFieldChange(InvField.UFV_TRANSIT_STATE) &&
-                    (UfvTransitStateEnum.S30_ECIN == inOriginalFieldChanges.findFieldChange(InvField.UFV_TRANSIT_STATE).getNewValue() ||
-                            UfvTransitStateEnum.S70_DEPARTED == inOriginalFieldChanges.findFieldChange(InvField.UFV_TRANSIT_STATE).getNewValue())) {
-
-                Unit unit;
-                UnitFacilityVisit unitFacilityVisit;
-                if (inEntity._entity instanceof Unit) {
-                    unit = (Unit) inEntity._entity;
-                    unitFacilityVisit = unit.getUnitActiveUfvNowActive();
-
-                } else {
-                    unitFacilityVisit = (UnitFacilityVisit) inEntity._entity;
-                    unit = unitFacilityVisit.getUfvUnit();
-                }
-
-                if (UfvTransitStateEnum.S30_ECIN == inOriginalFieldChanges.findFieldChange(InvField.UFV_TRANSIT_STATE).getNewValue() ||
-                        (UfvTransitStateEnum.S70_DEPARTED == inOriginalFieldChanges.findFieldChange(InvField.UFV_TRANSIT_STATE).getNewValue()
-                                && unitFacilityVisit.getUfvObCv()
-                                && LocTypeEnum.TRUCK == unitFacilityVisit.getUfvObCv().getCvCarrierMode())) {
-
-                    LOGGER.debug("send drayman message for unit : " + unit);
-                    Object library = getLibrary(LIBRARY);
-                    library.prepareAndPushMessageForPositionChange(unit, unitFacilityVisit.getUfvLastKnownPosition());
-                }
-            }*/
         } catch (Exception e) {
             LOGGER.error("Exception in onUpdate: " + e.getMessage());
         }
     }
 
-
+    private static Date getLfdDate(String dt) throws ParseException {
+        Calendar cal = Calendar.getInstance()
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MMM-dd")
+        if (dt.endsWith("!")) {
+            dt = dt.replaceAll("!", "")
+        }
+        if (dt.contains("no")) {
+            return null
+        }
+        cal.setTime(dateFormat.parse(dt))
+        return cal.getTime()
+    }
     private static final String T__IN_ENTITY = "inEntity";
     private static final String T__IN_TRIGGER_TYPE = "inTriggerType";
     private static final String T__IN_ORIGINAL_FIELD_CHANGES = "inOriginalFieldChanges";
@@ -72,7 +142,8 @@ class ITSUnitELI extends AbstractEntityLifecycleInterceptor {
     private static final String T__ON_UPDATE = "onUpdate";
     private final String T__ON_BOARD_UNIT_UPDATE = "OnboardUnitUpdate";
 
-
-    private static final String LIBRARY = "ITSDraymanGateAdaptor";
+    private ServicesManager servicesManager = (ServicesManager) Roastery.getBean(ServicesManager.BEAN_ID)
+    private static final String LIBRARY = "ITSAdaptor";
     private static final Logger LOGGER = Logger.getLogger(ITSUnitELI.class);
+
 }
