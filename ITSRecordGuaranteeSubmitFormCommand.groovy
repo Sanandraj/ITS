c5d977f8faf6182e4679115d1448d4343d7b88d3
AshokkumarKD <104904543+AshokkumarKD@users.noreply.github.com>
2023-01-18 18:54:54 +0530

    IP-409 final review done

--- ITSRecordGuaranteeSubmitFormCommand.groovy
+++ ITSRecordGuaranteeSubmitFormCommand.groovy
@@ -17,37 +17,36 @@ import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
 /**
- * @Author <a href="mailto:kgopinath@weservetech.com">Gopinath K</a>, 29/Dec/2022
+ * @Author: mailto:kgopinath@weservetech.com, Gopinath Kannappan; Date: 29/12/2022
  *
- * Requirements : 7-9 Apply Waiver to Multiple Units -- This groovy is used to record the guarantee for multiple units.
+ *  Requirements: 7-9 Apply Waiver to Multiple Units -- This groovy is used to record the guarantee for multiple units.
  *
  * @Inclusion Location: Incorporated as a code extension of the type
  *
  *  Load Code Extension to N4:
- *   1. Go to Administration --> System -->  Code Extension
- *   2. Click Add (+)
- *   3. Enter the values as below:
- Code Extension Name:  ITSRecordGuaranteeSubmitFormCommand
- Code Extension Type:  FORM_SUBMISSION_INTERCEPTOR
- Groovy Code: Copy and paste the contents of groovy code.
- *   4. Click Save button
+ *  1. Go to Administration --> System --> Code Extensions
+ *  2. Click Add (+)
+ *  3. Enter the values as below:
+ *     Code Extension Name: ITSRecordGuaranteeSubmitFormCommand
+ *     Code Extension Type: FORM_SUBMISSION_INTERCEPTOR
+ *     Groovy Code: Copy and paste the contents of groovy code.
+ *  4. Click Save button
  *
- * <formSubmissionCodeExtension name="ITSRecordGuaranteeSubmitFormCommand"/>
+ * @Setup In the database backend variform CUSTOM_INV_FORM_RECORD_GUARANTEE add <formSubmissionCodeExtension name="ITSRecordGuaranteeSubmitFormCommand"/>
  *
  *
  *  S.No    Modified Date   Modified By     Jira      Description
+ *
  */
 
 
 class ITSRecordGuaranteeSubmitFormCommand extends AbstractFormSubmissionCommand {
 
 
-    private static final Logger LOGGER = Logger.getLogger(ITSRecordGuaranteeSubmitFormCommand.class);
-
     @Override
     void doBeforeSubmit(String inVariformId, EntityId inEntityId, List<Serializable> inGkeys, EFieldChanges inOutFieldChanges, EFieldChanges inNonDbFieldChanges, Map<String, Object> inParams) {
         LOGGER.setLevel(Level.DEBUG)
-        LOGGER.debug("ITSRecordWaiverSubmitFormCommand started execution!!!!!!!!!!!!doBeforeSubmit method")
+        LOGGER.debug("ITSRecordGuaranteeSubmitFormCommand started execution!!!!!!!!!!!!doBeforeSubmit method")
 
         /* code to check  whether Enddate is less than StartDate.if it is true ,code will will popup a error message.*/
         FieldChanges fieldChanges = (FieldChanges) inOutFieldChanges
@@ -67,7 +66,6 @@ class ITSRecordGuaranteeSubmitFormCommand extends AbstractFormSubmissionCommand 
         }
 
 
-        LOGGER.debug("before submit inOutFieldChanges" + inOutFieldChanges)
     }
 
     @Override
@@ -81,14 +79,16 @@ class ITSRecordGuaranteeSubmitFormCommand extends AbstractFormSubmissionCommand 
         paramMap.put("GKEYS", inGkeys)
 
         EFieldChanges efc = inNonDbFieldChanges
-        efc.setFieldChange(SKIP_AFTER_SUBMIT,"No")
+        efc.setFieldChange(SKIP_AFTER_SUBMIT, "No")
 
         MessageCollector messageCollector = executeInTransaction("ITSRecordGuaranteeTransactionCallback", paramMap, results)
         if (messageCollector.hasError()) {
-            efc.setFieldChange(SKIP_AFTER_SUBMIT,"Yes")
+            efc.setFieldChange(SKIP_AFTER_SUBMIT, "Yes")
             registerMessageCollector(messageCollector)
         }
     }
 
     private static MetafieldId SKIP_AFTER_SUBMIT = MetafieldIdFactory.valueOf("skipaftersubmit");
+    private static Logger LOGGER = Logger.getLogger(ITSRecordGuaranteeSubmitFormCommand.class)
+
 }
